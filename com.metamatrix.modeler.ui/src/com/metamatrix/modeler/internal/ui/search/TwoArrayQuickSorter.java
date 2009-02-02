d3ad4e39d1fd9974167a0847b0c84f0cb8e1ada0
Barry Lafond <blafond@redhat.com>
2009-02-02 18:11:07 +0000

    Restored correct copyright
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@52 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- com.metamatrix.modeler.ui/src/com/metamatrix/modeler/internal/ui/search/TwoArrayQuickSorter.java
+++ com.metamatrix.modeler.ui/src/com/metamatrix/modeler/internal/ui/search/TwoArrayQuickSorter.java
@@ -1,23 +1,15 @@
-/*
- * JBoss, Home of Professional Open Source.
- *
- * See the LEGAL.txt file distributed with this work for information regarding copyright ownership and licensing.
- *
- * See the AUTHORS.txt file distributed with this work for a full listing of individual contributors.
- */
-package com.metamatrix.modeler.internal.ui.search;
-
-
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+
+package com.metamatrix.modeler.internal.ui.search;
 
 import java.util.Comparator;
 import org.eclipse.core.runtime.Assert;
