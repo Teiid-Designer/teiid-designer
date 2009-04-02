2d2905bbca2737d2ca15ff2ecf54df9222578c1b
John Doyle <jdoyle@redhat.com>
2009-04-02 18:00:50 +0000

    Fixing paths to resources.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@170 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- org.teiid.designer.modelgenerator.salesforce/src/com/metamatrix/modeler/modelgenerator/salesforce/connection/impl/Messages.java
+++ org.teiid.designer.modelgenerator.salesforce/src/com/metamatrix/modeler/modelgenerator/salesforce/connection/impl/Messages.java
@@ -13,7 +13,7 @@ import java.util.ResourceBundle;
 public class Messages {
 	private static final String PACKAGE_ID = Messages.class.getPackage().getName();
 	
-	private static final String I18N_NAME = PACKAGE_ID + "messages"; //$NON-NLS-1$
+	private static final String I18N_NAME = PACKAGE_ID + ".messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(I18N_NAME);
