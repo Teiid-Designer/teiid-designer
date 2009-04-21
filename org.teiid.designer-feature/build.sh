d12db25c51bd969adcd42167f883c85be2330cdd
John Verhaeg <jverhaeg@redhat.com>
2009-04-21 20:34:58 +0000

    Removing unused build process artifacts and updating remaining build process files in feature project.  Still unfinished and not working, but getting better...
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@209 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- org.teiid.designer-feature/build.sh
+++ org.teiid.designer-feature/build.sh
@@ -1,1 +1,2 @@
-ant -propertyfile build.ant.properties clean build.update.jar zip.distribution
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
+ant -debug -propertyfile build.ant.properties clean build.update.jar zip.distribution
