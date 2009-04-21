46e2658cc06b67f29ed391d4e97a7cda75930d5f
John Verhaeg <jverhaeg@redhat.com>
2009-04-21 21:13:31 +0000

    Removed unused code
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@212 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- org.teiid.designer.dqp.ui/src/com/metamatrix/modeler/dqp/ui/workspace/QueryClient.java
+++ org.teiid.designer.dqp.ui/src/com/metamatrix/modeler/dqp/ui/workspace/QueryClient.java
@@ -9,15 +9,10 @@ package com.metamatrix.modeler.dqp.ui.workspace;
 
 import java.io.File;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
-
 import net.sourceforge.sqlexplorer.plugin.SQLExplorerPlugin;
-
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-
 import com.metamatrix.admin.api.embedded.EmbeddedAdmin;
 import com.metamatrix.admin.api.embedded.EmbeddedLogger;
 import com.metamatrix.admin.api.exception.AdminException;
@@ -29,94 +24,98 @@ import com.metamatrix.modeler.dqp.internal.config.DqpPath;
 import com.metamatrix.modeler.dqp.ui.DqpUiConstants;
 import com.metamatrix.vdb.edit.loader.VDBConstants;
 
-
-/** 
+/**
  * A factory class to create connections to the embedded query from the Designer.
  */
 public class QueryClient {
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("-HH_mm_ss_MMM_dd"); //$NON-NLS-1$
+
     private Connection adminConnection;
-    
+
     protected Connection getAdminConnection() throws SQLException {
         if (this.adminConnection == null) {
-        	ClassLoader current = Thread.currentThread().getContextClassLoader();
+            ClassLoader current = Thread.currentThread().getContextClassLoader();
             try {
                 File propertiesDir = DqpPath.getRuntimePath().toFile();
                 String url = buildConnectionURL(propertiesDir.getAbsolutePath(), "admin", "1", new Properties()); //$NON-NLS-1$ //$NON-NLS-2$ 
                 DqpPlugin.Util.log(IStatus.INFO, "starting workspace execution with url = \"" + url); //$NON-NLS-1$ 
                 EmbeddedDriver driver = new EmbeddedDriver();
-                
+
                 Thread.currentThread().setContextClassLoader(driver.getClass().getClassLoader());
-                
+
                 this.adminConnection = (com.metamatrix.jdbc.api.Connection)driver.connect(url, null);
-                
+
                 // wire the logging so that the messages are flown into designer
                 ((EmbeddedAdmin)this.adminConnection.getAdminAPI()).setLogListener(new DesignerLogger());
             } catch (AdminException e) {
-            	throw new RuntimeException(e);
-            }  finally {
-            	Thread.currentThread().setContextClassLoader(current);
+                throw new RuntimeException(e);
+            } finally {
+                Thread.currentThread().setContextClassLoader(current);
             }
         }
         return this.adminConnection;
     }
-    
-        
+
     /**
-     * Obtain a string version of a URL suitable for the EmbeddedDriver. 
+     * Obtain a string version of a URL suitable for the EmbeddedDriver.
+     * 
      * @param theVdbName the VDB name
      * @param theVersion the VDB version
      * @param propsFile the path (including file name) of the DQP properties file
      * @return
      */
-    public String buildConnectionURL(String executionDir, String theVdbName, String theVersion, Properties executionProps) {
+    public String buildConnectionURL( String executionDir,
+                                      String theVdbName,
+                                      String theVersion,
+                                      Properties executionProps ) {
         String txnAutoWrap = executionProps.getProperty(VDBConstants.VDBElementNames.ExecutionProperties.Properties.TXN_AUTO_WRAP);
 
         File propsFile = new File(executionDir, "workspace.properties"); //$NON-NLS-1$
-        
+
         StringBuffer sb = new StringBuffer().append("jdbc:metamatrix:") //$NON-NLS-1$
-                                 .append(theVdbName)
-                                 .append('@')
-                                 .append("mmrofile:") //$NON-NLS-1$
-                                 .append(StringUtil.replaceAll(propsFile.getAbsolutePath(), "\\", "/")) //$NON-NLS-1$ //$NON-NLS-2$
-                                 .append(";version=") //$NON-NLS-1$
-                                 .append(theVersion)
-                                 .append(";XMLFormat=Tree;"); //$NON-NLS-1$
-        
+        .append(theVdbName).append('@').append("mmrofile:") //$NON-NLS-1$
+        .append(StringUtil.replaceAll(propsFile.getAbsolutePath(), "\\", "/")) //$NON-NLS-1$ //$NON-NLS-2$
+        .append(";version=") //$NON-NLS-1$
+        .append(theVersion).append(";XMLFormat=Tree;"); //$NON-NLS-1$
+
         if (SQLExplorerPlugin.getDefault() != null && SQLExplorerPlugin.getDefault().shouldShowQueryPlan()) {
             sb.append("sqlOptions").append("=").append("SHOWPLAN;"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         }
 
         sb.append("EmbeddedContext").append("=").append("Designer;"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         //sb.append("metamatrix.security.password.PasswordKeyStore").append("=").append(DqpPlugin.getInstance().getDqpKey()); //$NON-NLS-1$ //$NON-NLS-2$
-        
+
         if (txnAutoWrap != null) {
             sb.append("txnAutoWrap").append("=").append(txnAutoWrap).append(";"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         }
-        
+
         return sb.toString();
     }
-    
-    static class DesignerLogger implements EmbeddedLogger{
 
-		@Override
-		public void log(int logLevel, long timestamp, String componentName, String threadName, String message, Throwable throwable) {
-			switch(logLevel) {
-			case NONE:
-				break;
-			case CRITICAL:
-			case ERROR:
-				DqpUiConstants.UTIL.log(new Status(IStatus.ERROR, DqpUiConstants.PLUGIN_ID, IStatus.OK, message, throwable));
-				break;
-			case WARNING:
-				DqpUiConstants.UTIL.log(new Status(IStatus.WARNING, DqpUiConstants.PLUGIN_ID, IStatus.OK, message, throwable));
-				break;
-			case INFO:
-			case DETAIL:
-			case TRACE:
-				DqpUiConstants.UTIL.log(new Status(IStatus.INFO, DqpUiConstants.PLUGIN_ID, IStatus.OK, message, throwable));
-				break;				
-			}
-		}
+    static class DesignerLogger implements EmbeddedLogger {
+
+        @Override
+        public void log( int logLevel,
+                         long timestamp,
+                         String componentName,
+                         String threadName,
+                         String message,
+                         Throwable throwable ) {
+            switch (logLevel) {
+                case NONE:
+                    break;
+                case CRITICAL:
+                case ERROR:
+                    DqpUiConstants.UTIL.log(new Status(IStatus.ERROR, DqpUiConstants.PLUGIN_ID, IStatus.OK, message, throwable));
+                    break;
+                case WARNING:
+                    DqpUiConstants.UTIL.log(new Status(IStatus.WARNING, DqpUiConstants.PLUGIN_ID, IStatus.OK, message, throwable));
+                    break;
+                case INFO:
+                case DETAIL:
+                case TRACE:
+                    DqpUiConstants.UTIL.log(new Status(IStatus.INFO, DqpUiConstants.PLUGIN_ID, IStatus.OK, message, throwable));
+                    break;
+            }
+        }
     }
 }
