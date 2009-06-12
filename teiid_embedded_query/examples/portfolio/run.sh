6eb174e3b5242ac7220b1011daa3a7b1242265ec
Barry Lafond <blafond@redhat.com>
2009-06-12 02:50:36 +0000

    6.1.0 Release - TEIID changes
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@284 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- teiid_embedded_query/examples/portfolio/run.sh
+++ teiid_embedded_query/examples/portfolio/run.sh
@@ -4,6 +4,6 @@
 CLIENT_PATH=java/*:PortfolioModel/
 
 #Second one for the JARs in Teiid embedded
-TEIID_PATH=../../teiid-6.0.0-client.jar:../../deploy:../../lib/*:../../extensions/*
+TEIID_PATH=../../teiid-6.1.0-client.jar:../../deploy:../../lib/patches/*:../../lib/*:../../extensions/*
 
 java -cp ${CLIENT_PATH}:${TEIID_PATH} JDBCClient "select * from CustomerAccount"
