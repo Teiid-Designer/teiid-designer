36f66077750e5fed6fd355190b98a65ee97671a2
Barry Lafond <blafond@redhat.com>
2010-01-29 17:13:22 +0000

    TEIIDDES-255 - upgrading to second pass at stage 1 Teiid 7.0 integration.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@330 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- teiid_embedded_query/src/com/metamatrix/metadata/runtime/impl/FileRecordImpl.java
+++ teiid_embedded_query/src/com/metamatrix/metadata/runtime/impl/FileRecordImpl.java
@@ -32,11 +32,11 @@ import com.metamatrix.modeler.core.index.IndexSelector;
 import com.metamatrix.modeler.core.metadata.runtime.FileRecord;
 import com.metamatrix.modeler.internal.core.workspace.ModelFileUtil;
 
-/** 
+/**
  * @since 4.2
  */
 public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord {
-    
+
     /**
      */
     private static final long serialVersionUID = 1L;
@@ -45,7 +45,7 @@ public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord
     private String[] tokenReplacements;
     private IndexSelector selector;
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#getPathInVdb()
      * @since 4.2
      */
@@ -53,29 +53,29 @@ public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord
         return this.pathInVdb;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#isBinary()
      * @since 4.2
      */
     public boolean getBinary() {
-        if(this.pathInVdb != null && this.pathInVdb.endsWith(IndexConstants.INDEX_EXT)) {
+        if (this.pathInVdb != null && this.pathInVdb.endsWith(IndexConstants.INDEX_EXT)) {
             return true;
         }
         return false;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#getContent()
      * @since 4.2
      */
     public InputStream getContent() {
-        if(this.tokens != null) {
+        if (this.tokens != null) {
             return this.selector.getFileContent(getPathInVdb(), this.tokens, this.tokenReplacements);
         }
-        return this.selector.getFileContent(getPathInVdb());        
+        return this.selector.getFileContent(getPathInVdb());
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#getFileLength()
      * @since 4.2
      */
@@ -83,15 +83,15 @@ public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord
         return this.selector.getFileSize(getPathInVdb());
     }
 
-    /** 
+    /**
      * @param pathInVdb The pathInVdb to set.
      * @since 4.2
      */
-    public void setPathInVdb(final String pathInVdb) {
+    public void setPathInVdb( final String pathInVdb ) {
         this.pathInVdb = pathInVdb;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#getTokenReplacementString1()
      * @since 4.2
      */
@@ -99,37 +99,39 @@ public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord
         return this.tokens;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#getTokenReplacements()
      * @since 4.2
      */
     public String[] getTokenReplacements() {
         return this.tokenReplacements;
     }
-    /** 
+
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#setTokens(java.lang.String[])
      * @since 4.2
      */
-    public void setTokens(final String[] tokens) {
+    public void setTokens( final String[] tokens ) {
         this.tokens = tokens;
     }
-    /** 
+
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#setTokenReplacementString2(java.lang.String[])
      * @since 4.2
      */
-    public void setTokenReplacements(final String[] tokenReplacements) {
+    public void setTokenReplacements( final String[] tokenReplacements ) {
         this.tokenReplacements = tokenReplacements;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#setIndexSelector(com.metamatrix.modeler.core.index.IndexSelector)
      * @since 4.2
      */
-    public void setIndexSelector(final IndexSelector selector) {
+    public void setIndexSelector( final IndexSelector selector ) {
         this.selector = selector;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#getFileRecord()
      * @since 4.2
      */
@@ -137,35 +139,35 @@ public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord
         return this;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.MetadataRecord#getModelName()
      * @since 4.2
      */
     @Override
     public String getModelName() {
-        if(isModelFile()) {
-        	return FileUtils.getBaseFileNameWithoutExtension(this.pathInVdb);
+        if (isModelFile()) {
+            return FileUtils.getBaseFileNameWithoutExtension(this.pathInVdb);
         }
         return null;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#isIndexFile()
      * @since 4.2
      */
     public boolean isIndexFile() {
-        if(this.pathInVdb != null && this.pathInVdb.endsWith(IndexConstants.INDEX_EXT)) {
+        if (this.pathInVdb != null && this.pathInVdb.endsWith(IndexConstants.INDEX_EXT)) {
             return true;
         }
         return false;
     }
 
-    /** 
+    /**
      * @see com.metamatrix.modeler.core.metadata.runtime.FileRecord#getModelName()
      * @since 4.2
      */
     public boolean isModelFile() {
-        if(this.pathInVdb != null) {
+        if (this.pathInVdb != null) {
             File fileInVdb = this.selector.getFile(this.pathInVdb);
             return ModelFileUtil.isModelFile(fileInVdb);
         }
@@ -176,30 +178,34 @@ public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord
      * Compare two records for equality.
      */
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals( Object obj ) {
 
-        if(obj == this) {
+        if (obj == this) {
             return true;
         }
 
-        if(obj == null || obj.getClass() != this.getClass()) {
+        if (obj == null || obj.getClass() != this.getClass()) {
             return false;
         }
 
         FileRecord other = (FileRecord)obj;
 
-        if(!EquivalenceUtil.areEqual(this.getPathInVdb(), other.getPathInVdb())) { return false; }
-        if(!EquivalenceUtil.areEqual(this.getTokens(), other.getTokens())) { return false; }
-        if(!EquivalenceUtil.areEqual(this.getTokenReplacements(), other.getTokenReplacements())) { return false; }
+        if (!EquivalenceUtil.areEqual(this.getPathInVdb(), other.getPathInVdb())) {
+            return false;
+        }
+        if (!EquivalenceUtil.areEqual(this.getTokens(), other.getTokens())) {
+            return false;
+        }
+        if (!EquivalenceUtil.areEqual(this.getTokenReplacements(), other.getTokenReplacements())) {
+            return false;
+        }
 
         return true;
     }
 
     /**
-     * Get hashcode for From.  WARNING: The hash code relies on the variables
-     * in the record, so changing the variables will change the hash code, causing
-     * a select to be lost in a hash structure.  Do not hash a record if you plan
-     * to change it.
+     * Get hashcode for From. WARNING: The hash code relies on the variables in the record, so changing the variables will change
+     * the hash code, causing a select to be lost in a hash structure. Do not hash a record if you plan to change it.
      */
     @Override
     public int hashCode() {
@@ -208,15 +214,15 @@ public class FileRecordImpl extends AbstractMetadataRecord implements FileRecord
             myHash = HashCodeUtil.hashCode(myHash, this.pathInVdb);
         }
         if (this.tokens != null) {
-            myHash = HashCodeUtil.hashCode(myHash, this.tokens);
+            myHash = HashCodeUtil.hashCode(myHash, (Object[])this.tokens);
         }
         if (this.tokenReplacements != null) {
-            myHash = HashCodeUtil.hashCode(myHash, this.tokenReplacements);
+            myHash = HashCodeUtil.hashCode(myHash, (Object[])this.tokenReplacements);
         }
 
         return myHash;
     }
-    
+
     @Override
     public String toString() {
         return this.getPathInVdb();
