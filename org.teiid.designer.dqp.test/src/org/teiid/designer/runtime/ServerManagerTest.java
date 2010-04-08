002a339076e264dd379e2b1aa800e8e3a9e2e062
John Verhaeg <jverhaeg@redhat.com>
2010-04-08 01:51:13 +0000

    Updated to only test ServerManager
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@497 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- org.teiid.designer.dqp.test/src/org/teiid/designer/runtime/ServerManagerTest.java
+++ org.teiid.designer.dqp.test/src/org/teiid/designer/runtime/ServerManagerTest.java
@@ -13,12 +13,13 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.stub;
 import static org.mockito.Mockito.verify;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 /**
@@ -26,26 +27,8 @@ import org.mockito.MockitoAnnotations;
  */
 public class ServerManagerTest {
     private static final String MODEL1 = "Model1";
-    private static final String MODEL2 = "Model2";
-    private static final String MODEL3 = "Model3";
-    private static final String MODEL4 = "Model4";
-    private static final String MODEL5 = "Model5";
-
-    private static final String CONNECTOR_TYPE1 = "ConnectorType1";
-    private static final String CONNECTOR_TYPE2 = "ConnectorType2";
-    private static final String CONNECTOR_TYPE3 = "ConnectorType3";
-    private static final String CONNECTOR_TYPE4 = "ConnectorType4";
-    private static final String CONNECTOR_TYPE5 = "ConnectorType5";
 
     private static final String CONNECTOR1 = "Connector1";
-    private static final String CONNECTOR2 = "Connector2";
-    private static final String CONNECTOR3 = "Connector3";
-    private static final String CONNECTOR4 = "Connector4";
-    private static final String CONNECTOR5 = "Connector5";
-    private static final String CONNECTOR6 = "Connector6";
-    private static final String CONNECTOR7 = "Connector7";
-    private static final String CONNECTOR8 = "Connector8";
-    private static final String CONNECTOR9 = "Connector9";
 
     private static final String RESTORED_SERVER1_URL = "mm://localhost:8080";
     private static final String RESTORED_SERVER1_USER = "user8080";
@@ -57,38 +40,114 @@ public class ServerManagerTest {
     private static final String RESTORED_SERVER3_USER = "user8280";
 
     private static final String SERVER1_URL = "mm://server:4321";
-    private static final Server SERVER1;
 
     private ServerManager mgr;
 
-    static {
-        Map<String, String> connectorNameConnectorTypeNameMap = new HashMap<String, String>();
-        connectorNameConnectorTypeNameMap.put(CONNECTOR1, CONNECTOR_TYPE1);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR2, CONNECTOR_TYPE2);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR3, CONNECTOR_TYPE3);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR4, CONNECTOR_TYPE4);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR5, CONNECTOR_TYPE5);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR6, CONNECTOR_TYPE1);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR7, CONNECTOR_TYPE1);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR8, CONNECTOR_TYPE1);
-        connectorNameConnectorTypeNameMap.put(CONNECTOR9, CONNECTOR_TYPE1);
-
-        Map<String, String> modelNameConectorNameMap = new HashMap<String, String>();
-        modelNameConectorNameMap.put(MODEL1, CONNECTOR1);
-        modelNameConectorNameMap.put(MODEL2, CONNECTOR1);
-        modelNameConectorNameMap.put(MODEL3, CONNECTOR1);
-        modelNameConectorNameMap.put(MODEL4, CONNECTOR1);
-        modelNameConectorNameMap.put(MODEL5, CONNECTOR1);
-
-        SERVER1 = new TestServer(SERVER1_URL, "userA", "pwdA", false, mock(EventManager.class),
-                                 connectorNameConnectorTypeNameMap, modelNameConectorNameMap);
-    }
+    @Mock
+    private Server server1;
 
     @Before
     public void beforeEach() throws Exception {
         MockitoAnnotations.initMocks(this);
         this.mgr = new ServerManager(null);
-        SERVER1.getAdmin().refresh();
+        // server1.getAdmin().refresh();
+    }
+
+    @Test
+    public void shouldAllowGetServers() {
+        this.mgr.getServers();
+    }
+
+    @Test
+    public void shouldAllowSaveState() {
+        this.mgr.saveState();
+    }
+
+    @Test
+    public void shouldConfirmRegisteredListenerIsNotified() {
+        // create and register listener
+        final IExecutionConfigurationListener listener = mock(IExecutionConfigurationListener.class);
+        this.mgr.addListener(listener);
+
+        // generate event
+        this.mgr.addServer(server1);
+
+        // test to make sure listener was called once
+        verify(listener).configurationChanged((ExecutionConfigurationEvent)anyObject());
+    }
+
+    @Test
+    public void shouldConfirmServerIsNotAddedMultipleTimes() {
+        // add
+        this.mgr.addServer(server1);
+        assertThat(this.mgr.getServers().size(), is(1));
+
+        // add again
+        this.mgr.addServer(server1);
+        assertThat(this.mgr.getServers().size(), is(1));
+    }
+
+    @Test
+    public void shouldConfirmServerIsNotRegistered() {
+        assertThat(this.mgr.isRegistered(server1), is(false));
+    }
+
+    @Test
+    public void shouldConfirmServerIsRegistered() {
+        stub(server1.hasSameKey(server1)).toReturn(true);
+        assertThat(this.mgr.addServer(server1).isOK(), is(true));
+        assertThat(this.mgr.isRegistered(server1), is(true));
+    }
+
+    @Test
+    public void shouldConfirmServerIsRemoved() {
+        // first add
+        stub(server1.hasSameKey(server1)).toReturn(true);
+        this.mgr.addServer(server1);
+        assertThat(this.mgr.isRegistered(server1), is(true));
+
+        // now remove
+        assertThat(this.mgr.removeServer(server1).isOK(), is(true));
+        assertThat(this.mgr.isRegistered(server1), is(false));
+    }
+
+    @Test
+    public void shouldConfirmUnregisteredListenerIsNotNotified() {
+        // create and register listener
+        final IExecutionConfigurationListener listener = mock(IExecutionConfigurationListener.class);
+        this.mgr.addListener(listener);
+        this.mgr.removeListener(listener);
+
+        // generate event
+        this.mgr.addServer(server1);
+
+        // test to make sure listener was called once
+        verify(listener, never()).configurationChanged((ExecutionConfigurationEvent)anyObject());
+    }
+
+    @Test
+    public void shouldFindConnectorsForModel() throws Exception {
+        // register the server
+        final ExecutionAdmin admin = mock(ExecutionAdmin.class);
+        stub(server1.getAdmin()).toReturn(admin);
+        final SourceBindingsManager srcBindingsMgr = mock(SourceBindingsManager.class);
+        stub(admin.getSourceBindingsManager()).toReturn(srcBindingsMgr);
+        final Connector connector = mock(Connector.class);
+        stub(srcBindingsMgr.getConnectorsForModel(MODEL1)).toReturn(Collections.singleton(connector));
+        stub(connector.getName()).toReturn(CONNECTOR1);
+        this.mgr.addServer(server1);
+
+        // test
+        final Collection<Connector> connectors = this.mgr.getConnectorsForModel(MODEL1);
+        assertThat(connectors.size(), is(1));
+        assertThat(connectors.iterator().next().getName(), is(CONNECTOR1));
+    }
+
+    @Test
+    public void shouldGetServerByUrl() {
+        this.mgr.addServer(server1);
+        stub(server1.getUrl()).toReturn(SERVER1_URL);
+        assertThat(this.mgr.getServer(SERVER1_URL), is(server1));
     }
 
     @Test( expected = IllegalArgumentException.class )
@@ -96,31 +155,14 @@ public class ServerManagerTest {
         this.mgr.addListener(null);
     }
 
-    @Test
-    public void shouldConfirmRegisteredListenerIsNotified() {
-        // create and register listener
-        IExecutionConfigurationListener listener = mock(IExecutionConfigurationListener.class);
-        this.mgr.addListener(listener);
-
-        // generate event
-        this.mgr.addServer(SERVER1);
-
-        // test to make sure listener was called once
-        verify(listener).configurationChanged((ExecutionConfigurationEvent)anyObject());
+    @Test( expected = IllegalArgumentException.class )
+    public void shouldNotAllowNullListenerForRemoveListener() {
+        this.mgr.removeListener(null);
     }
 
-    @Test
-    public void shouldConfirmUnregisteredListenerIsNotNotified() {
-        // create and register listener
-        IExecutionConfigurationListener listener = mock(IExecutionConfigurationListener.class);
-        this.mgr.addListener(listener);
-        this.mgr.removeListener(listener);
-
-        // generate event
-        this.mgr.addServer(SERVER1);
-
-        // test to make sure listener was called once
-        verify(listener, never()).configurationChanged((ExecutionConfigurationEvent)anyObject());
+    @Test( expected = IllegalArgumentException.class )
+    public void shouldNotAllowNullReplacedServerForUpdateServer() {
+        this.mgr.updateServer(null, null);
     }
 
     @Test( expected = IllegalArgumentException.class )
@@ -129,57 +171,23 @@ public class ServerManagerTest {
     }
 
     @Test( expected = IllegalArgumentException.class )
-    public void shouldNotAllowNullUrlForGetServer() {
-        this.mgr.getServer(null);
-    }
-
-    @Test
-    public void shouldAllowGetServers() {
-        this.mgr.getServers();
-    }
-
-    @Test( expected = IllegalArgumentException.class )
     public void shouldNotAllowNullServerForIsRegistered() {
         this.mgr.isRegistered(null);
     }
 
-    @Test
-    public void shouldConfirmServerIsRegistered() {
-        assertThat(this.mgr.addServer(SERVER1).isOK(), is(true));
-        assertThat(this.mgr.isRegistered(SERVER1), is(true));
+    @Test( expected = IllegalArgumentException.class )
+    public void shouldNotAllowNullServerForRemoveServer() {
+        this.mgr.removeServer(null);
     }
 
-    @Test
-    public void shouldConfirmServerIsRemoved() {
-        // first add
-        this.mgr.addServer(SERVER1);
-        assertThat(this.mgr.isRegistered(SERVER1), is(true));
-
-        // now remove
-        assertThat(this.mgr.removeServer(SERVER1).isOK(), is(true));
-        assertThat(this.mgr.isRegistered(SERVER1), is(false));
+    @Test( expected = IllegalArgumentException.class )
+    public void shouldNotAllowNullUpdatedServerForUpdateServer() {
+        this.mgr.updateServer(mock(Server.class), null);
     }
 
-    @Test
-    public void shouldConfirmServerIsNotAddedMultipleTimes() {
-        // add
-        this.mgr.addServer(SERVER1);
-        assertThat(this.mgr.getServers().size(), is(1));
-
-        // add again
-        this.mgr.addServer(SERVER1);
-        assertThat(this.mgr.getServers().size(), is(1));
-    }
-
-    @Test
-    public void shouldConfirmServerIsNotRegistered() {
-        assertThat(this.mgr.isRegistered(SERVER1), is(false));
-    }
-
-    @Test
-    public void shouldGetServerByUrl() {
-        this.mgr.addServer(SERVER1);
-        assertThat(this.mgr.getServer(SERVER1_URL), is(SERVER1));
+    @Test( expected = IllegalArgumentException.class )
+    public void shouldNotAllowNullUrlForGetServer() {
+        this.mgr.getServer(null);
     }
 
     @Test
@@ -199,42 +207,6 @@ public class ServerManagerTest {
         server = this.mgr.getServer(RESTORED_SERVER3_URL);
         assertThat(server, notNullValue());
         assertThat(server.getUser(), is(RESTORED_SERVER3_USER));
-    }
-
-    @Test( expected = IllegalArgumentException.class )
-    public void shouldNotAllowNullServerForRemoveServer() {
-        this.mgr.removeServer(null);
-    }
-
-    @Test( expected = IllegalArgumentException.class )
-    public void shouldNotAllowNullListenerForRemoveListener() {
-        this.mgr.removeListener(null);
-    }
-
-    @Test( expected = IllegalArgumentException.class )
-    public void shouldNotAllowNullReplacedServerForUpdateServer() {
-        this.mgr.updateServer(null, null);
-    }
-
-    @Test( expected = IllegalArgumentException.class )
-    public void shouldNotAllowNullUpdatedServerForUpdateServer() {
-        this.mgr.updateServer(mock(Server.class), null);
-    }
-
-    @Test
-    public void shouldAllowSaveState() {
-        this.mgr.saveState();
-    }
-
-    @Test
-    public void shouldFindConnectorsForModel() throws Exception {
-        // register the server
-        this.mgr.addServer(SERVER1);
-
-        // test
-        Collection<Connector> connectors = this.mgr.getConnectorsForModel(MODEL1);
-        assertThat(connectors.size(), is(1));
-        assertThat(connectors.iterator().next().getName(), is(CONNECTOR1));
     }
 
 }
