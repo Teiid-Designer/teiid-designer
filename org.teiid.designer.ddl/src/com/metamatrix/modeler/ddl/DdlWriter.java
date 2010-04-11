7633c1c16e6e09536805f667e8257534daa403ec
Barry Lafond <blafond@redhat.com>
2010-04-11 03:24:01 +0000

    TEIIDDES-261 moving inherited Teiid code to own plugin. Fixed MANIFEST.MF's
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@508 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- org.teiid.designer.ddl/src/com/metamatrix/modeler/ddl/DdlWriter.java
+++ org.teiid.designer.ddl/src/com/metamatrix/modeler/ddl/DdlWriter.java
@@ -13,6 +13,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.resource.Resource;
 import com.metamatrix.core.log.Logger;
 import com.metamatrix.modeler.core.workspace.ModelResource;
+import com.metamatrix.modeler.core.workspace.ModelWorkspace;
 import com.metamatrix.modeler.core.workspace.ModelWorkspaceSelections;
 
 /**
@@ -22,64 +23,72 @@ public interface DdlWriter {
 
     /**
      * Get the logger that this writer is using.
-     * @return the logger; never null, but may be a {@link com.metamatrix.core.log.NullLogger NullLogger}
-     * if there is no logging.
+     * 
+     * @return the logger; never null, but may be a {@link com.metamatrix.core.log.NullLogger NullLogger} if there is no logging.
      */
     public Logger getLogger();
 
     /**
      * Set the logger that this writer should use.
-     * @param logger the new logger; may be null or a {@link com.metamatrix.core.log.NullLogger NullLogger}
-     * if there is to be no logging.
+     * 
+     * @param logger the new logger; may be null or a {@link com.metamatrix.core.log.NullLogger NullLogger} if there is to be no
+     *        logging.
      */
-    public void setLogger(Logger logger);
-    
+    public void setLogger( Logger logger );
+
     /**
      * Get the options that this writer is currently using
+     * 
      * @return the options; never null
      */
     public DdlOptions getOptions();
 
     /**
      * Write out the supplied model as DDL to the supplied stream.
-     * @param emfResource the {@link com.metamatrix.metamodels.relational.RelationalPackage relational} 
-     * EMF resource that contains the model to be written out; may not be null
+     * 
+     * @param emfResource the {@link com.metamatrix.metamodels.relational.RelationalPackage relational} EMF resource that contains
+     *        the model to be written out; may not be null
      * @param modelName the name of the model in the resource; may not be null
      * @param modelFilename the name of the model resource
      * @param stream the stream to which the DDL is to be written; may not be null
      * @param monitor the monitor the should be used; may be null
-     * @return a status of the process with any {@link IStatus#WARNING warnings}, 
-     * {@link IStatus#ERROR errors} or {@link IStatus#INFO information messages}, or 
-     * which will be {@link IStatus#isOK() marked as OK} if there were
-     * no warnings, errors or other messages.
+     * @return a status of the process with any {@link IStatus#WARNING warnings}, {@link IStatus#ERROR errors} or
+     *         {@link IStatus#INFO information messages}, or which will be {@link IStatus#isOK() marked as OK} if there were no
+     *         warnings, errors or other messages.
      */
-    public IStatus write( Resource emfResource, String modelName, String modelFilename,
-                          OutputStream stream, IProgressMonitor monitor );
-    
+    public IStatus write( Resource emfResource,
+                          String modelName,
+                          String modelFilename,
+                          OutputStream stream,
+                          IProgressMonitor monitor );
+
     /**
      * Write out the supplied model as DDL to the supplied stream.
-     * @param model the {@link com.metamatrix.metamodels.relational.RelationalPackage relational} model 
-     * that is to be written out; may not be null
+     * 
+     * @param model the {@link com.metamatrix.metamodels.relational.RelationalPackage relational} model that is to be written out;
+     *        may not be null
      * @param stream the stream to which the DDL is to be written; may not be null
      * @param monitor the monitor the should be used; may be null
-     * @return a status of the process with any {@link IStatus#WARNING warnings}, 
-     * {@link IStatus#ERROR errors} or {@link IStatus#INFO information messages}, or 
-     * which will be {@link IStatus#isOK() marked as OK} if there were
-     * no warnings, errors or other messages.
+     * @return a status of the process with any {@link IStatus#WARNING warnings}, {@link IStatus#ERROR errors} or
+     *         {@link IStatus#INFO information messages}, or which will be {@link IStatus#isOK() marked as OK} if there were no
+     *         warnings, errors or other messages.
      */
-    public IStatus write(ModelResource model, OutputStream stream, IProgressMonitor monitor );
-    
+    public IStatus write( ModelResource model,
+                          OutputStream stream,
+                          IProgressMonitor monitor );
+
     /**
-     * Write out the selected objects in the {@link ModelWorkspace model workspace}
-     * as DDL to the supplied stream.
-     * @param model the {@link com.metamatrix.metamodels.relational.RelationalPackage relational} model 
-     * that is to be written out; may not be null
+     * Write out the selected objects in the {@link ModelWorkspace model workspace} as DDL to the supplied stream.
+     * 
+     * @param model the {@link com.metamatrix.metamodels.relational.RelationalPackage relational} model that is to be written out;
+     *        may not be null
      * @param stream the stream to which the DDL is to be written; may not be null
      * @param monitor the monitor the should be used; may be null
-     * @return a status of the process with any {@link IStatus#WARNING warnings}, 
-     * {@link IStatus#ERROR errors} or {@link IStatus#INFO information messages}, or 
-     * which will be {@link IStatus#isOK() marked as OK} if there were
-     * no warnings, errors or other messages.
+     * @return a status of the process with any {@link IStatus#WARNING warnings}, {@link IStatus#ERROR errors} or
+     *         {@link IStatus#INFO information messages}, or which will be {@link IStatus#isOK() marked as OK} if there were no
+     *         warnings, errors or other messages.
      */
-    public IStatus write(ModelWorkspaceSelections selections, OutputStream stream, IProgressMonitor monitor );
+    public IStatus write( ModelWorkspaceSelections selections,
+                          OutputStream stream,
+                          IProgressMonitor monitor );
 }
