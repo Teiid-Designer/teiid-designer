54b11c8dd528fd816191bff6d257c990073a707c
Barry Lafond <blafond@redhat.com>
2010-06-25 15:59:06 +0000

    TEIIDDES-479 adding exception logging to static initializer
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@737 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.query.ui/src/com/metamatrix/query/internal/ui/sqleditor/sql/SqlSyntax.java
+++ plugins/org.teiid.designer.query.ui/src/com/metamatrix/query/internal/ui/sqleditor/sql/SqlSyntax.java
@@ -18,6 +18,7 @@ import org.teiid.designer.udf.UdfManager;
 import org.teiid.query.function.FunctionForm;
 import org.teiid.query.function.FunctionLibrary;
 import com.metamatrix.query.sql.ReservedWords;
+import com.metamatrix.query.ui.UiPlugin;
 
 /**
  * This class provides static Lists of SQL syntax words. RESERVED_WORDS contains reserved SQL words, FUNCTION_NAMES contains
@@ -39,51 +40,67 @@ public class SqlSyntax {
     public static final List<String> ALL_WORD_START_CHARS = new ArrayList<String>();
 
     static {
-        // RESERVED WORDS List
-        for (int i = 0; i != ReservedWords.ALL_WORDS.length; ++i) {
-            String reservedWord = ReservedWords.ALL_WORDS[i];
-            RESERVED_WORDS.add(reservedWord);
-            String start = reservedWord.substring(0, 1);
-            if (!RESERVED_WORD_START_CHARS.contains(start)) {
-                RESERVED_WORD_START_CHARS.add(start);
-            }
-        }
-        Collections.sort(RESERVED_WORDS);
+    	
+    	
+        try {
+			// RESERVED WORDS List
+			for (int i = 0; i != ReservedWords.ALL_WORDS.length; ++i) {
+			    String reservedWord = ReservedWords.ALL_WORDS[i];
+			    RESERVED_WORDS.add(reservedWord);
+			    String start = reservedWord.substring(0, 1);
+			    if (!RESERVED_WORD_START_CHARS.contains(start)) {
+			        RESERVED_WORD_START_CHARS.add(start);
+			    }
+			}
+			Collections.sort(RESERVED_WORDS);
+		} catch (Exception e) {
+			UiPlugin.Util.log(e);
+		}
 
-        // FUNCTION NAMES List
-        FunctionLibrary functionLib = UdfManager.INSTANCE.getFunctionLibrary();
-        List allCategories = functionLib.getFunctionCategories();
         Set allFunctionNames = new HashSet();
-        Iterator iter = allCategories.iterator();
-        while (iter.hasNext()) {
-            String catName = (String)iter.next();
-            List catFunctions = functionLib.getFunctionForms(catName);
-            Iterator funcIter = catFunctions.iterator();
-            while (funcIter.hasNext()) {
-                FunctionForm fForm = (FunctionForm)funcIter.next();
-                String fName = fForm.getName();
-                allFunctionNames.add(fName);
-                String start = fName.substring(0, 1);
-                if (!FUNCTION_NAME_START_CHARS.contains(start)) {
-                    FUNCTION_NAME_START_CHARS.add(start);
-                }
-            }
-        }
-        FUNCTION_NAMES.addAll(allFunctionNames);
-        Collections.sort(FUNCTION_NAMES);
+		Iterator iter;
+		try {
+			// FUNCTION NAMES List
+			FunctionLibrary functionLib = UdfManager.INSTANCE.getFunctionLibrary();
+			List allCategories = functionLib.getFunctionCategories();
+			iter = allCategories.iterator();
+			while (iter.hasNext()) {
+			    String catName = (String)iter.next();
+			    List catFunctions = functionLib.getFunctionForms(catName);
+			    Iterator funcIter = catFunctions.iterator();
+			    while (funcIter.hasNext()) {
+			        FunctionForm fForm = (FunctionForm)funcIter.next();
+			        String fName = fForm.getName();
+			        allFunctionNames.add(fName);
+			        String start = fName.substring(0, 1);
+			        if (!FUNCTION_NAME_START_CHARS.contains(start)) {
+			            FUNCTION_NAME_START_CHARS.add(start);
+			        }
+			    }
+			}
+			FUNCTION_NAMES.addAll(allFunctionNames);
+			Collections.sort(FUNCTION_NAMES);
+		} catch (Exception e) {
+			UiPlugin.Util.log(e);
+		}
 
         // DATATYPE NAMES List
-        Set<String> dataTypes = DataTypeManager.getAllDataTypeNames();
-        iter = dataTypes.iterator();
-        while (iter.hasNext()) {
-            String dtypeName = (String)iter.next();
-            DATATYPE_NAMES.add(dtypeName);
-            String start = dtypeName.substring(0, 1);
-            if (!DATATYPE_NAME_START_CHARS.contains(start)) {
-                DATATYPE_NAME_START_CHARS.add(start);
-            }
-        }
-        Collections.sort(DATATYPE_NAMES);
+		Set<String> dataTypes = new HashSet<String>();
+		try {
+			dataTypes = DataTypeManager.getAllDataTypeNames();
+			iter = dataTypes.iterator();
+			while (iter.hasNext()) {
+			    String dtypeName = (String)iter.next();
+			    DATATYPE_NAMES.add(dtypeName);
+			    String start = dtypeName.substring(0, 1);
+			    if (!DATATYPE_NAME_START_CHARS.contains(start)) {
+			        DATATYPE_NAME_START_CHARS.add(start);
+			    }
+			}
+			Collections.sort(DATATYPE_NAMES);
+		} catch (Exception e) {
+			UiPlugin.Util.log(e);
+		}
 
         // ALL WORDS List
         Set allWords = new HashSet();
