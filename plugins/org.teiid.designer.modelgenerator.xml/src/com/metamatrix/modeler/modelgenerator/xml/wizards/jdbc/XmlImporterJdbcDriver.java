46bdd8fd605b44f8671d0f61e1c30735e9736b68
John Doyle <jdoyle@redhat.com>
2010-07-09 14:17:53 +0000

    Adding new EObject method to fix 3.6 compile.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@802 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.modelgenerator.xml/src/com/metamatrix/modeler/modelgenerator/xml/wizards/jdbc/XmlImporterJdbcDriver.java
+++ plugins/org.teiid.designer.modelgenerator.xml/src/com/metamatrix/modeler/modelgenerator/xml/wizards/jdbc/XmlImporterJdbcDriver.java
@@ -7,11 +7,13 @@
  */
 package com.metamatrix.modeler.modelgenerator.xml.wizards.jdbc;
 
+import java.lang.reflect.InvocationTargetException;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -267,6 +269,17 @@ public class XmlImporterJdbcDriver implements JdbcDriver {
 	public void eNotify(Notification notification) {
 		// TODO Auto-generated method stub
 
-	}
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.emf.ecore.EObject#eInvoke(org.eclipse.emf.ecore.EOperation, org.eclipse.emf.common.util.EList)
+     */
+    @Override
+    public Object eInvoke( EOperation arg0,
+                           EList<?> arg1 ) throws InvocationTargetException {
+        return null;
+    }
 
 }
