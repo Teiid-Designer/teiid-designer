54481f532b78654857559d820512f55aead9d728
Ted Jones <tejones@redhat.com>
2010-07-26 17:36:48 +0000

    TEIIDDES-430 - Cleaning up (removed unused variables)
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@836 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.dqp.ui/src/com/metamatrix/modeler/dqp/ui/DqpUiPlugin.java
+++ plugins/org.teiid.designer.dqp.ui/src/com/metamatrix/modeler/dqp/ui/DqpUiPlugin.java
@@ -12,7 +12,6 @@ import java.util.ResourceBundle;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -23,7 +22,6 @@ import org.osgi.framework.BundleContext;
 import com.metamatrix.core.PluginUtil;
 import com.metamatrix.core.util.I18nUtil;
 import com.metamatrix.core.util.PluginUtilImpl;
-import com.metamatrix.modeler.core.workspace.ModelProject;
 import com.metamatrix.ui.AbstractUiPlugin;
 import com.metamatrix.ui.actions.ActionService;
 
@@ -33,26 +31,6 @@ import com.metamatrix.ui.actions.ActionService;
 public class DqpUiPlugin extends AbstractUiPlugin implements DqpUiConstants {
 
     private static final String PREFIX = I18nUtil.getPropertyPrefix(DqpUiPlugin.class);
-    /**
-     * The install location of the <code>modeler.transformation</code> plugin.
-     * 
-     * @since 6.0.0
-     */
-    private IPath installPath;
-
-    /**
-     * The project where the workspace UDF model is located.
-     * 
-     * @since 6.0.0
-     */
-    private ModelProject udfProject;
-
-    /**
-     * The workspace location of the <code>modeler.transformation</code> plugin.
-     * 
-     * @since 6.0.0
-     */
-    private IPath runtimePath;
 
     /**
      * Used in non-Eclipse environments to identify the install location of the <code>modeler.transformation</code> plugin.
