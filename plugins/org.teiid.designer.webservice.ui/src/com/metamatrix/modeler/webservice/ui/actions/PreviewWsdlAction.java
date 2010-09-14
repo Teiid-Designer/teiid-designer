4f51dc7045929dcb8f72e3d8f91cab75d7ac39ae
Ted Jones <tejones@redhat.com>
2010-09-14 15:14:19 +0000

    TEIIDDES-562: Changed the Generate WSDL action to a Preview WSDL action as a contextual menu option off of a web service model.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/branches/7.1@986 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.webservice.ui/src/com/metamatrix/modeler/webservice/ui/actions/PreviewWsdlAction.java
+++ plugins/org.teiid.designer.webservice.ui/src/com/metamatrix/modeler/webservice/ui/actions/PreviewWsdlAction.java
@@ -1,5 +1,7 @@
 package com.metamatrix.modeler.webservice.ui.actions;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,8 +16,18 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.xsd.XSDSchema;
 import org.eclipse.xsd.util.XSDParser;
 import com.metamatrix.metamodels.relational.Procedure;
@@ -29,15 +41,19 @@ import com.metamatrix.modeler.internal.webservice.gen.BasicWsdlGenerator;
 import com.metamatrix.modeler.ui.actions.SortableSelectionAction;
 import com.metamatrix.modeler.webservice.ui.IUiConstants;
 import com.metamatrix.ui.internal.eventsupport.SelectionUtilities;
+import com.metamatrix.ui.internal.widget.Dialog;
+import com.metamatrix.ui.text.ScaledFontManager;
 
-public class GenerateWsdlAction extends SortableSelectionAction {
+public class PreviewWsdlAction extends SortableSelectionAction {
 
-    public List<ModelResource> wsResources = new ArrayList();
-    public static String WSDL_GENERATION = "WSDL Generation"; //$NON-NLS-1$
+    public List<ModelResource> wsResources = new ArrayList<ModelResource>();
+    public static String WSDL_GENERATION = "WSDL Preview"; //$NON-NLS-1$
     public static String WSDL_GENERATION_SUCCESS = "Successfully generated WSDL file: "; //$NON-NLS-1$
     public static String WSDL_GENERATION_ERROR = "There was an error generating the WSDL..."; //$NON-NLS-1$
+    public static String WSDL_DIALOG_TITLE = "WSDL Preview"; //$NON-NLS-1$
+        
 
-    public GenerateWsdlAction() {
+    public PreviewWsdlAction() {
         super();
     }
 
@@ -45,12 +61,10 @@ public class GenerateWsdlAction extends SortableSelectionAction {
     public void run() {
 
         BasicWsdlGenerator wsdlGenerator = new BasicWsdlGenerator();
-        ModelResource wsModel = null;
         // This will be overwritten by the web service model name
         String webServiceName = "TeiidWS"; //$NON-NLS-1$
         for (ModelResource webServiceModel : wsResources) {
             try {
-                wsModel = webServiceModel;
                 wsdlGenerator.addWebServiceModel(webServiceModel.getEmfResource());
                 webServiceName = webServiceModel.getItemName();
                 IResource[] iResources = WorkspaceResourceFinderUtil.getDependentResources(webServiceModel.getResource());
@@ -72,33 +86,33 @@ public class GenerateWsdlAction extends SortableSelectionAction {
         wsdlGenerator.setUrlSuffixForReferences(""); //$NON-NLS-1$
         wsdlGenerator.setUrlForWsdlService("http://serverName:port/warName/"); //$NON-NLS-1$
         final IStatus status = wsdlGenerator.generate(new NullProgressMonitor());
-
+        
+        // Create a StringBuffer into which the WSDL can be written ...
+        final ByteArrayOutputStream bas = new ByteArrayOutputStream();
+        final BufferedOutputStream stream = new BufferedOutputStream(bas);
+        try {
+        	wsdlGenerator.write(stream);
+        } catch (IOException e) {
+            MessageDialog.openInformation(null, WSDL_GENERATION, WSDL_GENERATION_ERROR + e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            if ( stream != null ) {
+                try {
+					stream.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e.getMessage());
+				}
+            }
+        }
+        
         // nothing more to do if an error is expected
         if (status.getSeverity() == IStatus.ERROR) {
             ErrorDialog.openError(null, WSDL_GENERATION, WSDL_GENERATION_ERROR, status);
             throw new RuntimeException("Unable to generate WSDL"); //$NON-NLS-1$
         }
-
-        String fileName = webServiceName + "." + IUiConstants.WSDL_FILE_EXTENSION; //$NON-NLS-1$
-        try {
-            // Create our WSDL file and write to it
-            String path = wsModel.getResource().getLocation().toOSString();
-            OutputStream stream = new FileOutputStream(new File(path.substring(0, path.lastIndexOf("/")), fileName)); //$NON-NLS-1$
-            wsdlGenerator.write(stream);
-            // Get an iFile instance to refresh our workspace
-            IFile iFile = wsModel.getModelProject().getProject().getFile(fileName);
-            iFile.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-
-        } catch (IOException e) {
-            MessageDialog.openInformation(null, WSDL_GENERATION, WSDL_GENERATION_ERROR + e.getMessage());
-            throw new RuntimeException(e.getMessage());
-        } catch (CoreException e) {
-            MessageDialog.openInformation(null, WSDL_GENERATION, WSDL_GENERATION_ERROR + e.getMessage());
-            throw new RuntimeException(e.getMessage());
-        }
-
-        MessageDialog.openInformation(null, WSDL_GENERATION, WSDL_GENERATION_SUCCESS + fileName);
-
+        
+        new WsdlDialog(Display.getDefault().getActiveShell(), bas.toString()).open();
+        
     }
 
     public XSDSchema importSchema( String path ) {
@@ -151,5 +165,69 @@ public class GenerateWsdlAction extends SortableSelectionAction {
     @Override
     public boolean isApplicable( ISelection selection ) {
         return isValidSelection(selection);
+    }
+    
+    class WsdlDialog extends Dialog {
+
+        private StyledText text;
+        private String wsdlString;
+
+        /**
+         * Construct an instance of WsdlDialog.
+         */
+        public WsdlDialog(Shell shell, String wsdlString) {
+            super(shell, WSDL_DIALOG_TITLE);
+            this.wsdlString = wsdlString;
+        }
+
+        /* (non-Javadoc)
+         * @see org.eclipse.jface.window.Window#createDialogArea(org.eclipse.swt.widgets.Composite)
+         */
+        protected Control createDialogArea(Composite parent) {
+            Composite composite = (Composite)super.createDialogArea(parent);
+            // add controls to composite as necessary
+
+            text = new StyledText(composite, SWT.V_SCROLL);
+            GridData gd = new GridData(GridData.FILL_BOTH);
+            text.setLayoutData(gd);
+
+            text.setEditable(false);
+            text.setWordWrap(true);
+            text.setTabs(4);
+
+            StyleRange bodyRange = new StyleRange();
+            bodyRange.start = 0;
+            bodyRange.length = wsdlString.length();
+            ScaledFontManager fontManager = new ScaledFontManager();
+            text.setFont(fontManager.createFontOfSize(10));
+
+            text.setText(wsdlString);
+            text.setStyleRange(bodyRange);
+
+            super.setSizeRelativeToScreen(75, 70);
+
+            return composite;
+        }
+
+        /* (non-Javadoc)
+         * @see org.eclipse.jface.window.Window#create()
+         */
+        public void create() {
+            setShellStyle(getShellStyle() | SWT.RESIZE);
+            super.create();
+            super.getShell().setText(WSDL_DIALOG_TITLE);
+        }
+
+        /* (non-Javadoc)
+         * @see org.eclipse.jface.dialogs.Dialog#createButtonsForButtonBar(org.eclipse.swt.widgets.Composite)
+         */
+        protected void createButtonsForButtonBar(Composite parent) {
+            Button okButton = createButton(
+                parent,
+                IDialogConstants.OK_ID,
+                IDialogConstants.OK_LABEL,
+                true);
+            okButton.setFocus();
+        }
     }
 }
