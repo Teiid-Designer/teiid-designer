97953b84da53d48f8de436a41c07455a6f8375fd
Pavol Srna <psrna@redhat.com>
2011-03-02 16:31:21 +0000

    * fixed doubleclick action
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@1534 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- tests/org.teiid.designer.ui.bot.ext/src/org/teiid/designer/ui/bot/ext/teiid/SWTBotTeiidTFigure.java
+++ tests/org.teiid.designer.ui.bot.ext/src/org/teiid/designer/ui/bot/ext/teiid/SWTBotTeiidTFigure.java
@@ -1,6 +1,10 @@
 package org.teiid.designer.ui.bot.ext.teiid;
 
 import org.eclipse.draw2d.FigureCanvas;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
+import org.eclipse.swtbot.swt.finder.results.VoidResult;
 
 import com.metamatrix.modeler.transformation.ui.figure.TransformationFigure;
 
@@ -15,8 +19,42 @@ public class SWTBotTeiidTFigure extends SWTBotTeiidFigure{
 
 	@Override
 	public void doubleClick() {
-		doubleClickXY(tFig.getBounds().getCenter().x, 
-				      tFig.getBounds().getCenter().y);
+
+		UIThreadRunnable.syncExec(new VoidResult() {
+			
+			@Override
+			public void run() {
+								
+				// Move mouse
+				Event event = new Event();
+				event.type = SWT.MouseMove;
+				event.x = widget.toDisplay(tFig.getBounds().getCenter().x, tFig.getBounds().getCenter().y).x;
+				event.y = widget.toDisplay(tFig.getBounds().getCenter().x, tFig.getBounds().getCenter().y).y;
+				widget.getDisplay().post(event);
+
+				// Mouse down
+				event = new Event();
+				event.type = SWT.MouseDown;
+				event.button = 1;
+				widget.getDisplay().post(event);
+				// Mouse Up
+				event = new Event();
+				event.type = SWT.MouseUp;
+				event.button = 1;
+				widget.getDisplay().post(event);
+				// Mouse down
+				event = new Event();
+				event.type = SWT.MouseDown;
+				event.button = 1;
+				widget.getDisplay().post(event);
+				// Mouse Up
+				event = new Event();
+				event.type = SWT.MouseUp;
+				event.button = 1;
+				widget.getDisplay().post(event);
+				
+			}
+		});
 	}
 
 }
