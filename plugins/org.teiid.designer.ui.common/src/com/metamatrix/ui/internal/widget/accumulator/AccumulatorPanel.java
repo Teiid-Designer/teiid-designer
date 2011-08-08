2ba9c7ec7690b2956002724fc24eac1e21968f38
Mark Drilling <mdrillin@redhat.com>
2011-08-08 18:19:27 +0000

    TEIIDDES-843 - Adjusted width hints on accumulator panel left and right tables - dialog was too wide.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/branches/7.4.2@1808 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.ui.common/src/com/metamatrix/ui/internal/widget/accumulator/AccumulatorPanel.java
+++ plugins/org.teiid.designer.ui.common/src/com/metamatrix/ui/internal/widget/accumulator/AccumulatorPanel.java
@@ -44,7 +44,7 @@ public class AccumulatorPanel extends Composite implements InternalUiConstants {
     // Static variables
     // ============================================================
     final public static int DEFAULT_RESET_BUTTON_MARGIN_HEIGHT = 10;
-    final public static int DEFAULT_LEFT_SIDE_WIDTH_HINT = (int)(Display.getDefault().getBounds().width * .25);
+    final public static int DEFAULT_LEFT_SIDE_WIDTH_HINT = (int)(Display.getDefault().getBounds().width * .10);
     final public static int DEFAULT_RIGHT_SIDE_WIDTH_HINT = DEFAULT_LEFT_SIDE_WIDTH_HINT;
     final public static int DEFAULT_LEFT_AND_RIGHT_SIDE_HEIGHT_HINT = (int)(Display.getDefault().getBounds().height * .10);
 
@@ -119,12 +119,12 @@ public class AccumulatorPanel extends Composite implements InternalUiConstants {
         if (leftSideWidthHint < 1) {
             this.leftSideWidthHint = DEFAULT_LEFT_SIDE_WIDTH_HINT;
         } else {
-        this.leftSideWidthHint = leftSideWidthHint;
+            this.leftSideWidthHint = leftSideWidthHint;
         }
         if (rightSideWidthHint < 1) {
             this.rightSideWidthHint = DEFAULT_RIGHT_SIDE_WIDTH_HINT;
         } else {
-        this.rightSideWidthHint = rightSideWidthHint;
+            this.rightSideWidthHint = rightSideWidthHint;
         }
         if (leftAndRightSideHeightHint < 1) {
             this.leftAndRightSideHeightHint = DEFAULT_LEFT_AND_RIGHT_SIDE_HEIGHT_HINT;
@@ -402,7 +402,7 @@ public class AccumulatorPanel extends Composite implements InternalUiConstants {
         // Initialize changeListeners to empty
         changeListeners = new ArrayList();
     }
-    
+
     public void addAccumulatedValuesChangeListener( IAccumulatedValuesChangeListener listener ) {
         changeListeners.add(listener);
     }
