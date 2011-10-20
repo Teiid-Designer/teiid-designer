f736faca6ba92500d66cabad59c75e74c23a2ba2
Dan Florian <dflorian@redhat.com>
2011-10-20 17:39:24 +0000

    Add test plugin for org.teiid.designer.extension. Fixed a mistake in the constants file.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@2015 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- tests/org.teiid.designer.extension.test/src/org/teiid/designer/extension/Constants.java
+++ tests/org.teiid.designer.extension.test/src/org/teiid/designer/extension/Constants.java
@@ -20,16 +20,16 @@ public interface Constants {
 
     String TESTDATA = "testdata"; //$NON-NLS-1$
 
-    String MED_SCHEMA = TESTDATA + File.separatorChar + ExtensionConstants.SCHEMA_FILENAME;
-    String SALESFORCE_MED = TESTDATA + File.separatorChar + "salesforce.mxd"; //$NON-NLS-1$
-
     String FUNCTION_METAMODEL = "http://www.metamatrix.com/metamodels/MetaMatrixFunction"; //$NON-NLS-1$
     String RELATIONAL_METAMODEL = "http://www.metamatrix.com/metamodels/Relational"; //$NON-NLS-1$
     String WEB_SERVICE_METAMODEL = "http://www.metamatrix.com/metamodels/WebService"; //$NON-NLS-1$
     String XML_METAMODEL = "http://www.metamatrix.com/metamodels/XmlDocument"; //$NON-NLS-1$
 
+    String MED_SCHEMA = TESTDATA + File.separatorChar + ExtensionConstants.SCHEMA_FILENAME;
+    String SALESFORCE_MED = TESTDATA + File.separatorChar + "salesforce.mxd"; //$NON-NLS-1$
+
     String SALESFORCE_MED_PREFIX = "salesforce"; //$NON-NLS-1$
-    String SALESFORCE_MED_URI = "salesforce"; //$NON-NLS-1$
+    String SALESFORCE_MED_URI = "org.teiid.designer.extension.salesforce"; //$NON-NLS-1$
 
     String DEFAULT_MED_DESCRIPTION = "This is a MED description"; //$NON-NLS-1$
     String DEFAULT_NAMESPACE_PREFIX = SALESFORCE_MED_PREFIX;
