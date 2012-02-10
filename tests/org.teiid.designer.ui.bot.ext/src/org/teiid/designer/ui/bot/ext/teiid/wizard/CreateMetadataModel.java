8c647ba0bc66a8a631769d8662036af2fc362659
Lucia Jelinkova <ljelinko@redhat.com>
2012-02-10 16:50:18 +0000

    added constants
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@2236 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- tests/org.teiid.designer.ui.bot.ext/src/org/teiid/designer/ui/bot/ext/teiid/wizard/CreateMetadataModel.java
+++ tests/org.teiid.designer.ui.bot.ext/src/org/teiid/designer/ui/bot/ext/teiid/wizard/CreateMetadataModel.java
@@ -1,5 +1,9 @@
 package org.teiid.designer.ui.bot.ext.teiid.wizard;
 
+import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.jboss.tools.ui.bot.ext.SWTBotFactory;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
 import org.jboss.tools.ui.bot.ext.wizards.SWTBotNewObjectWizard;
 
 /**
@@ -10,24 +14,68 @@ import org.jboss.tools.ui.bot.ext.wizards.SWTBotNewObjectWizard;
  */
 public class CreateMetadataModel {
 
+	public static class ModelClass {
+
+		public static final String RELATIONAL = "Relational";
+
+		public static final String XML = "XML";
+
+		public static final String XSD = "XML Schema (XSD)";
+
+		public static final String WEBSERVICE = "Web Service";
+
+		public static final String MODEL_EXTENSION = "Model Extension";
+
+		public static final String FUNCTION = "Function";
+
+	}
+	
+	public static class ModelType {
+
+		public static final String SOURCE = "Source Model";
+
+		public static final String VIEW = "View Model";
+
+		public static final String DATATYPE = "Datatype Model";
+
+		public static final String EXTENSION = "Model Class Extension";
+
+		public static final String FUNCTION = "User Defined Function";
+
+	}
+
 	private SWTBotNewObjectWizard wizard = new SWTBotNewObjectWizard();
-	
+
 	private String location;
-	
+
 	private String name;
-	
+
+	private String clazz;
+
 	private String type;
-	
+
 	public void execute(){
 		wizard.open("Teiid Metadata Model", "Teiid Designer");
 		fillFirstPage();
 		wizard.finishWithWait();
+
+		xsdSchemaSelection();
 	}
 
 	private void fillFirstPage() {
 		wizard.bot().textWithLabel("Location:").setText(location);
 		wizard.bot().textWithLabel("Model Name:").setText(name);
+		wizard.bot().comboBoxWithLabel("Model Class:").setSelection(clazz);
 		wizard.bot().comboBoxWithLabel("Model Type:").setSelection(type);		
+	}
+
+	private void xsdSchemaSelection() {
+		if (ModelClass.XSD.equals(clazz)){
+			SWTBotShell shell = SWTBotFactory.getBot().shell("Model Initializer");
+			shell.bot().table().select("XML Schema (2001)");
+			shell.bot().button(IDELabel.Button.OK).click();
+			SWTBotFactory.getBot().waitUntil(Conditions.shellCloses(shell));
+		}
 	}
 
 	public void setLocation(String location) {
@@ -40,5 +88,9 @@ public class CreateMetadataModel {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	public void setClass(String clazz){
+		this.clazz = clazz;
 	}
 }
