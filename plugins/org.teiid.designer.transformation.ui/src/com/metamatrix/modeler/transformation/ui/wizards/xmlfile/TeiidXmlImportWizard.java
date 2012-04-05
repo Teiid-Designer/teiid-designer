fb4bbb2220cff26e13baed2309552868994c2b2a
Barry Lafond <blafond@redhat.com>
2012-04-05 18:06:07 +0000

    TEIIEDDES-1290 added null check on designerProperties before setting values
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/trunk@2440 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.transformation.ui/src/com/metamatrix/modeler/transformation/ui/wizards/xmlfile/TeiidXmlImportWizard.java
+++ plugins/org.teiid.designer.transformation.ui/src/com/metamatrix/modeler/transformation/ui/wizards/xmlfile/TeiidXmlImportWizard.java
@@ -73,8 +73,10 @@ public class TeiidXmlImportWizard extends TeiidMetadataImportWizard {
 		}, false);
 		
         // Update Properties to include the created source and view Models
-        DesignerPropertiesUtil.setSourceModelName(this.designerProperties, getFileInfo().getSourceModelName());
-        DesignerPropertiesUtil.setViewModelName(this.designerProperties, getFileInfo().getViewModelName());
+		if( this.designerProperties != null ) {
+            DesignerPropertiesUtil.setSourceModelName(this.designerProperties, getFileInfo().getSourceModelName());
+            DesignerPropertiesUtil.setViewModelName(this.designerProperties, getFileInfo().getViewModelName());
+		}
 
 		return true;
 	}
