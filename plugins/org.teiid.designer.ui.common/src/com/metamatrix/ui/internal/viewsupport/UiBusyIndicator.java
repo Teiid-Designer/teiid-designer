2dd60fce14f145fdbf510ccce39988d75c3c6c81
Barry Lafond <blafond@redhat.com>
2012-05-17 20:30:02 +0000

    TEIIDDES-1374 backed out fix for tomorrows build. Designer would hang/sleep on start-up.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/branches/7.7.x@2567 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.ui.common/src/com/metamatrix/ui/internal/viewsupport/UiBusyIndicator.java
+++ plugins/org.teiid.designer.ui.common/src/com/metamatrix/ui/internal/viewsupport/UiBusyIndicator.java
@@ -8,6 +8,7 @@
 package com.metamatrix.ui.internal.viewsupport;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -53,7 +54,83 @@ public abstract class UiBusyIndicator {
 
     private static int nextBusyId = 1;
     private static final String BUSYID_NAME = "UI BusyIndicator"; //$NON-NLS-1$
+    // ===============================================================
+    /**
+     * Runs the given <code>Runnable</code> on the Display thread while providing
+     * busy feedback using this busy indicator.
+     * 
+     * @param display the display on which the busy feedback should be
+     *        displayed.  If the display is null, the Display for the current
+     *        thread will be used.  If there is no Display for the current thread,
+     *        the runnable code will be executed and no busy feedback will be displayed.
+     * @param runnable the runnable for which busy feedback is to be shown.
+     *        Must not be null.
+     * 
+    * @exception IllegalArgumentException <ul>
+     *    <li>ERROR_NULL_ARGUMENT - if the runnable is null</li>
+     * </ul>
+     */
+    public static void showWhile(Display display, final Runnable runnable) {
 
+        // ensure runnable is not null
+        if (runnable == null)
+            SWT.error(SWT.ERROR_NULL_ARGUMENT);
+        
+        // ensure we can get a valid Display object
+        if (display == null) {
+            display = Display.getCurrent();
+            if (display == null) {
+                runnable.run();
+                return;
+            }
+        }
+        
+        // ensure this method is called only on the display's event dispatch thread
+        if ( display.getThread() != Thread.currentThread() ) {
+            final Display d = display;
+            display.syncExec(new Runnable() {
+                public void run() {
+                    showWhile(d, runnable);
+                }
+            });
+            return;
+        }
+    
+        Integer busyId = new Integer(nextBusyId);
+        nextBusyId++;
+        Cursor cursor = new Cursor(display, SWT.CURSOR_WAIT);
+    
+        
+        Shell[] shells = display.getShells();
+        for (int i = 0; i < shells.length; i++) {
+            Integer id = (Integer)shells[i].getData(BUSYID_NAME);
+            if (id == null) {
+                shells[i].setCursor(cursor);
+                shells[i].setData(BUSYID_NAME, busyId);
+            }
+        }
+        
+        try {
+            if ( display.getThread() == Thread.currentThread() ) {
+                runnable.run();
+            } else {
+                display.syncExec(runnable);
+            }
+        } finally {
+            shells = display.getShells();
+            for (int i = 0; i < shells.length; i++) {
+                Integer id = (Integer)shells[i].getData(BUSYID_NAME);
+                if (busyId.equals(id)) {
+                    shells[i].setCursor(null);
+                    shells[i].setData(BUSYID_NAME, null);
+                }
+            }
+            if (!cursor.isDisposed()) {
+                cursor.dispose();
+            }
+        }
+    }
+    // =======================================================
     /**
      * Runs the given <code>Runnable</code> on the Display thread while
      * providing busy feedback using this busy indicator.
@@ -78,40 +155,40 @@ public abstract class UiBusyIndicator {
      *                <li>ERROR_NULL_ARGUMENT - if the runnable is null</li>
      *                </ul>
      */
-    public static synchronized void showWhile(Display display,
-            final Runnable runnable) {
-        // ensure runnable is not null
-        if (runnable == null)
-            SWT.error(SWT.ERROR_NULL_ARGUMENT);
-        
-        // ensure we can get a valid Display object
-        if (display == null) {
-            display = Display.getCurrent() != null ? Display.getCurrent()
-                    : Display.getDefault();
-            if (display == null) {
-                // This just should not happen ...
-                runnable.run();
-                return;
-            }
-        }
-
-        /*
-         * ensure this method is called only on the display's event dispatch
-         * thread
-         */
-        if (display.getThread() != Thread.currentThread()) {
-            final Display d = display;
-            display.syncExec(new Runnable() {
-                @Override
-                public void run() {
-                    showWhileInternal(d, runnable);
-                }
-            });
-        }
-        else {
-            showWhileInternal(display, runnable);
-        }
-    }
+//    public static synchronized void showWhile(Display display,
+//            final Runnable runnable) {
+//        // ensure runnable is not null
+//        if (runnable == null)
+//            SWT.error(SWT.ERROR_NULL_ARGUMENT);
+//        
+//        // ensure we can get a valid Display object
+//        if (display == null) {
+//            display = Display.getCurrent() != null ? Display.getCurrent()
+//                    : Display.getDefault();
+//            if (display == null) {
+//                // This just should not happen ...
+//                runnable.run();
+//                return;
+//            }
+//        }
+//
+//        /*
+//         * ensure this method is called only on the display's event dispatch
+//         * thread
+//         */
+//        if (display.getThread() != Thread.currentThread()) {
+//            final Display d = display;
+//            display.syncExec(new Runnable() {
+//                @Override
+//                public void run() {
+//                    showWhileInternal(d, runnable);
+//                }
+//            });
+//        }
+//        else {
+//            showWhileInternal(display, runnable);
+//        }
+//    }
 
     /**
      * Internal method that change the cursor to WAITING, executies the runnable
