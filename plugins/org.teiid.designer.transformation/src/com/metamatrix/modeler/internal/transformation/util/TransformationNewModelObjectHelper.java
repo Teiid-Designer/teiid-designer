ebebd301183ce38dc93cf5e72766f1653ef04b95
Barry Lafond <blafond@redhat.com>
2012-05-17 19:19:18 +0000

    TEIIDDES-1363 rolling back the change. Jira rejected.
    git-svn-id: http://anonsvn.jboss.org/repos/tdesigner/branches/7.7.x@2566 ceac5f5e-c25f-0410-8b95-ec8c2776e4a6

--- plugins/org.teiid.designer.transformation/src/com/metamatrix/modeler/internal/transformation/util/TransformationNewModelObjectHelper.java
+++ plugins/org.teiid.designer.transformation/src/com/metamatrix/modeler/internal/transformation/util/TransformationNewModelObjectHelper.java
@@ -85,7 +85,6 @@ public class TransformationNewModelObjectHelper implements INewModelObjectHelper
 	                        tableAspect.setSupportsUpdate(newTarget, false);
 	                    } // endif
 
-	                    TransformationHelper.setSelectSqlString(newRoot, VIRTUAL_PROCEDURE_TEMPLATE_SQL, false, this);
 	                    return true;
                 	}
                 } else if (TransformationHelper.isSqlProcedure(newTarget) && !TransformationHelper.isOperation(newTarget)) {
