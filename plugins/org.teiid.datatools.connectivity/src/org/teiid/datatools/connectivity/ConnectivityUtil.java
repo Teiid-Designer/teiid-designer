afa08d0beccfa33b90e76ef4951d98ab45e5be4c
Paul Richardson <p.g.richardson@redhat.com>
2012-10-01 17:31:17 +0100

    TEIIDDES-1434: Store and retrieve JDBC passwords securely
    * Rather than storing the JDBC password in connection properties which are
      persisted in the clear, use Eclipse's secure storage instead
    
    * TeiidJDBCConnection
     * Retrieve the password from the secure storage
    
    * ConnectivityUtil
     * Add the username to the driver's properties
     * Store the password in secure storage
    
    * TeiidDriverUIContributor
     * UI that handles the display and modification of the connection's properties
     * Retrieve and store the JDBC password from and to secure storage

--- plugins/org.teiid.datatools.connectivity/src/org/teiid/datatools/connectivity/ConnectivityUtil.java
+++ plugins/org.teiid.datatools.connectivity/src/org/teiid/datatools/connectivity/ConnectivityUtil.java
@@ -8,7 +8,6 @@
 package org.teiid.datatools.connectivity;
 
 import java.util.Properties;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
@@ -65,7 +64,6 @@ public class ConnectivityUtil {
     public static Properties createDriverProps( String jarList,
                                                 String driverURL,
                                                 String username,
-                                                String password,
                                                 String vdbName ) {
         Properties baseProperties = new Properties();
         baseProperties.setProperty(IDriverMgmtConstants.PROP_DEFN_JARLIST, jarList);
@@ -73,9 +71,6 @@ public class ConnectivityUtil {
         baseProperties.setProperty(IJDBCDriverDefinitionConstants.URL_PROP_ID, driverURL);
         if(null != username) { 
         	baseProperties.setProperty(IJDBCDriverDefinitionConstants.USERNAME_PROP_ID, username);
-        }
-        if(password != null) {
-        	baseProperties.setProperty(IJDBCDriverDefinitionConstants.PASSWORD_PROP_ID, password);	
         }
         baseProperties.setProperty(IJDBCDriverDefinitionConstants.DATABASE_VENDOR_PROP_ID, TEIID_DATABASE_VENDOR_NAME);
         baseProperties.setProperty(IJDBCDriverDefinitionConstants.DATABASE_VERSION_PROP_ID, TEIID_DATABASE_VERSION);
@@ -98,21 +93,21 @@ public class ConnectivityUtil {
         try {
             DriverInstance mDriver = DriverManager.getInstance().getDriverInstanceByID(TEIID_PREVIEW_DRIVER_DEFINITION_ID);
             if (mDriver == null) {
-                createTeiidPreviewDriverInstance(driverPath, connectionURL);
+                createTeiidPreviewDriverInstance(driverPath, connectionURL, username);
             } else {
-            	// JBIDE-7493 Eclipse updates can break profiles because the driverPath is plugin version specific.
-            	String jarList = mDriver.getJarList();
-            	if(jarList != driverPath) {
-            		mDriver.getPropertySet().getBaseProperties().put(IDriverMgmtConstants.PROP_DEFN_JARLIST, driverPath);
-            	}
-            	mDriver.getPropertySet().getBaseProperties().put(IJDBCDriverDefinitionConstants.URL_PROP_ID, connectionURL);
+                // JBIDE-7493 Eclipse updates can break profiles because the driverPath is plugin version specific.
+                String jarList = mDriver.getJarList();
+                if(jarList != driverPath) {
+                    mDriver.getPropertySet().getBaseProperties().put(IDriverMgmtConstants.PROP_DEFN_JARLIST, driverPath);
+                }
+                mDriver.getPropertySet().getBaseProperties().put(IJDBCDriverDefinitionConstants.URL_PROP_ID, connectionURL);
             }
-            
+
+            storeJDBCPassword(connectionURL, password);
 
             return pm.createTransientProfile(TEIID_PROFILE_PROVIDER_ID, createDriverProps(driverPath,
                                                                                           connectionURL,
                                                                                           username,
-                                                                                          password,
                                                                                           vdbName));
         } catch (ConnectionProfileException e) {
             Status status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0, "error getting profile", e); //$NON-NLS-1$
@@ -122,13 +117,17 @@ public class ConnectivityUtil {
     }
 
     private static void createTeiidPreviewDriverInstance( String jarList,
-                                                          String driverURL ) {
+                                                          String driverURL, String username ) {
         IPropertySet pset = new PropertySetImpl(TEIID_PREVIEW_DRIVER_NAME, TEIID_PREVIEW_DRIVER_DEFINITION_ID);
         Properties baseProperties = new Properties();
         baseProperties.setProperty(IJDBCDriverDefinitionConstants.DRIVER_CLASS_PROP_ID, TEIID_DRIVER_NAME);
         if(null != driverURL) {
         	baseProperties.setProperty(IJDBCDriverDefinitionConstants.URL_PROP_ID, driverURL);
         }
+        if (null != username) {
+            baseProperties.setProperty(IJDBCDriverDefinitionConstants.USERNAME_PROP_ID, username);
+        }
+        
         baseProperties.setProperty(IJDBCDriverDefinitionConstants.DATABASE_VENDOR_PROP_ID, TEIID_DATABASE_VENDOR_NAME);
         baseProperties.setProperty(IJDBCDriverDefinitionConstants.DATABASE_VERSION_PROP_ID, TEIID_DATABASE_VERSION);
         baseProperties.setProperty(IDriverMgmtConstants.PROP_DEFN_JARLIST, jarList);
@@ -193,16 +192,18 @@ public class ConnectivityUtil {
 
     	DriverInstance mDriver = DriverManager.getInstance().getDriverInstanceByID(TEIID_DRIVER_DEFINITION_ID_BASE + vdbName);
         if (mDriver == null) {
-            createTeiidPreviewDriverInstance(driverPath, null);
+            createTeiidPreviewDriverInstance(driverPath, driverPath, username);
         } else {
 			// JBIDE-7493 Eclipse updates can break profiles because the driverPath is plugin version specific.
 			String jarList = mDriver.getJarList();
 			if(jarList != driverPath) {
 				mDriver.getPropertySet().getBaseProperties().put(IDriverMgmtConstants.PROP_DEFN_JARLIST, driverPath);
 			}
-		}
+        }
+        
+        storeJDBCPassword(connectionURL, password);
 
-		return createDriverProps(driverPath, connectionURL, username, password, vdbName);
+        return createDriverProps(driverPath, connectionURL, username, vdbName);
     }
     
     public static IConnectionProfile createVDBTeiidProfile( String driverPath,
@@ -215,7 +216,7 @@ public class ConnectivityUtil {
     	try {
     		DriverInstance mDriver = DriverManager.getInstance().getDriverInstanceByID(TEIID_PREVIEW_DRIVER_DEFINITION_ID);
     		if (mDriver == null) {
-    			createTeiidPreviewDriverInstance(driverPath, connectionURL);
+    			createTeiidPreviewDriverInstance(driverPath, connectionURL, username);
     		} else {
     			// JBIDE-7493 Eclipse updates can break profiles because the driverPath is plugin version specific.
     			String jarList = mDriver.getJarList();
@@ -223,6 +224,9 @@ public class ConnectivityUtil {
     				mDriver.getPropertySet().getBaseProperties().put(IDriverMgmtConstants.PROP_DEFN_JARLIST, driverPath);
     			}
     		}
+    		
+    		storeJDBCPassword(connectionURL, password);
+    		
     		IConnectionProfile existingCP = pm.getProfileByName(profileName);
     		if (existingCP != null) {
     			return existingCP;
@@ -230,11 +234,29 @@ public class ConnectivityUtil {
 
     		return pm.createProfile(profileName, "", //$NON-NLS-1$
     				TEIID_PROFILE_PROVIDER_ID,
-    				createDriverProps(driverPath, connectionURL, username, password, vdbName));
+    				createDriverProps(driverPath, connectionURL, username, vdbName));
     	} catch (ConnectionProfileException e) {
     		Status status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0, "error getting profile", e); //$NON-NLS-1$
     		throw new CoreException(status);
     	}
+    }
+    
+    /**
+     * Store the given password against the url using the built-in secure storage
+     * 
+     * @param url
+     * @param password
+     */
+    private static void storeJDBCPassword(String url, String password) {
+        if (password == null)
+            return;
+        
+        String urlStorageKey = ConnectivityUtil.buildSecureStorageKey(TeiidJDBCConnection.class, url);
+        try {
+            getSecureStorageProvider().storeInSecureStorage(urlStorageKey, ConnectivityUtil.JDBC_PASSWORD, password);
+        } catch (Exception ex) {
+            Activator.log(ex);
+        }
     }
     
     /**
