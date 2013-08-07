38e187218b921d32a24ea8944fed6f49e468596b
Daniel Florian <dflorian@redhat.com>
2013-08-07 12:39:15 -0400

    TEIIDDES-1821 Modeshape JDBC import option does not work Fixed some layout issues in importer UI, changed version to 3.4, fixed the way the REST response and parsing of repositories.

--- plugins/org.teiid.designer.datatools/src/org/teiid/designer/datatools/profiles/modeshape/RestUtils.java
+++ plugins/org.teiid.designer.datatools/src/org/teiid/designer/datatools/profiles/modeshape/RestUtils.java
@@ -7,13 +7,12 @@ import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.teiid.designer.core.util.StringUtilities;
 
 
 /**
@@ -21,6 +20,13 @@ import org.teiid.designer.core.util.StringUtilities;
  */
 public class RestUtils {
 
+	/**
+	 * @param url the ModeShape server URL
+	 * @param user the user ID
+	 * @param pass the password
+	 * @return a collection of repository names (never <code>null</code>)
+	 * @throws Exception if there is a problem obtaining the repository names
+	 */
 	public List<String> getRepositoryList(URL url, String user, String pass)
 			throws Exception {
 		Authenticator basicAuth = new BasicAuthenticator(user, pass);
@@ -32,9 +38,11 @@ public class RestUtils {
 	}
 
 	private static String getRepositories(URL url) throws Exception {
-		String result = StringUtilities.SPACE;
 		URLConnection conn;
 		conn = url.openConnection();
+
+		 // make sure we get a JSON response
+		conn.setRequestProperty("Accept", "application/json"); //$NON-NLS-1$ //$NON-NLS-2$
 
 		// Get the response
 		BufferedReader rd = new BufferedReader(new InputStreamReader(
@@ -45,25 +53,31 @@ public class RestUtils {
 			sb.append(line);
 		}
 		rd.close();
-		result = sb.toString();
-		return result;
+		return sb.toString();
 	}
 
-	private static List<String> parseRepositories(String jsonResponse)
-			throws Exception {
-		List<String> result = new ArrayList<String>();
-		if (!jsonResponse.isEmpty()) {
-			JsonFactory jsonFactory = new JsonFactory();
-			JsonParser jp = jsonFactory.createJsonParser(jsonResponse);
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode rootNode = mapper.readTree(jp);
-			for (JsonNode reposNode : rootNode) {
-				result.add(reposNode.path("repository").path("name") //$NON-NLS-1$ //$NON-NLS-2$
-						.getTextValue());
-			}
-		}
-		return result;
-	}
+    private static List<String> parseRepositories(final String jsonResponse) throws Exception {
+        if (!jsonResponse.isEmpty()) {
+            final JsonFactory jsonFactory = new JsonFactory();
+            final JsonParser jp = jsonFactory.createJsonParser(jsonResponse);
+            final ObjectMapper mapper = new ObjectMapper();
+            final JsonNode rootNode = mapper.readTree(jp);
+
+            // should only get one repositories node back
+            if ((rootNode != null) && (rootNode.size() == 1)) {
+                final JsonNode repositoriesNode = rootNode.iterator().next();
+                final List<String> result = new ArrayList<String>(repositoriesNode.size());
+
+                for (final JsonNode repoNode : repositoriesNode) {
+                    result.add(repoNode.path("name").getTextValue()); //$NON-NLS-1$
+                }
+
+                return result;
+            }
+        }
+
+        return Collections.emptyList();
+    }
 
 	private class BasicAuthenticator extends Authenticator {
 
