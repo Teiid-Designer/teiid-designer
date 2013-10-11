edd5ac2317603be964d95d6edf73e2e9f0101378
Paul Richardson <p.g.richardson@redhat.com>
2013-10-11 16:43:07 +0100

    TEIIDDES-305: Adds in statistics for performance optimisation
    * To optimise the query, the distinct value and null value counts are
      required. These properties can be set using the searches already
      conducted when displaying the child attributes in the column page tree
    
    * Adds fields to LdapAttributeNode for the null value count, distinct value
      count and maximum length of the attribute's values.

--- plugins/org.teiid.designer.modelgenerator.ldap.ui/src/org/teiid/designer/modelgenerator/ldap/ui/wizards/ILdapAttributeNode.java
+++ plugins/org.teiid.designer.modelgenerator.ldap.ui/src/org/teiid/designer/modelgenerator/ldap/ui/wizards/ILdapAttributeNode.java
@@ -13,6 +13,11 @@ package org.teiid.designer.modelgenerator.ldap.ui.wizards;
 public interface ILdapAttributeNode {
 
     /**
+     * Value used for the default length
+     */
+    int DEFAULT_VALUE_LENGTH = 32768;
+
+    /**
      * @return the id
      */
     String getId();
@@ -31,4 +36,31 @@ public interface ILdapAttributeNode {
      * @param label
      */
     void setLabel(String label);
+
+    /**
+     * @return distinct value count
+     */
+    int getDistinctValueCount();
+
+    /**
+     * Increment the null value count
+     */
+    void incrementNullValueCount();
+
+    /**
+     * @return null value count
+     */
+    int getNullValueCount();
+
+    /**
+     * Add a value of this attribute. Used for calculation of a distinct value count
+     *
+     * @param value
+     */
+    void addValue(Object value);
+
+    /**
+     * @return the length of the longest value
+     */
+    int getMaximumValueLength();
 }
