87a990efe5f2a52175a438b0a8450a35e3135cdc
mdrillin <mdrillin@redhat.com>
2013-11-15 12:35:48 -0600

    TEIIDDES-1918 Resolve type mapping issue in RelationalModelProcessor

--- plugins/org.teiid.designer.metamodels.relational/src/org/teiid/designer/metamodels/relational/util/RelationalTypeMappingImpl.java
+++ plugins/org.teiid.designer.metamodels.relational/src/org/teiid/designer/metamodels/relational/util/RelationalTypeMappingImpl.java
@@ -128,10 +128,16 @@ public class RelationalTypeMappingImpl implements RelationalTypeMapping {
 	public EObject getDatatype( final String jdbcTypeName ) throws ModelerCoreException {
     	EObject result = null;
         if (jdbcTypeName != null) {
-        	Integer typeCode = SQL_TYPE_MAPPING.get(jdbcTypeName.toUpperCase());
-	        if (typeCode != null) {
-	        	result = getDatatype(typeCode);
-	        }
+        	// Look for direct match to Built-in types first
+        	result = this.datatypeManager.getBuiltInDatatype(jdbcTypeName);
+
+        	// No direct match, so look for jdbc type
+        	if(result == null) {
+            	Integer typeCode = SQL_TYPE_MAPPING.get(jdbcTypeName.toUpperCase());
+    	        if (typeCode != null) {
+    	        	result = getDatatype(typeCode);
+    	        }
+        	}
         }
         if (result == null) {
             result = findDatatype(DatatypeConstants.BuiltInNames.OBJECT);
