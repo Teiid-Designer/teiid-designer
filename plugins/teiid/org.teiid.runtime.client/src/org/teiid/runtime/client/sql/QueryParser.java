2a87dd5d2cf381cad3047489fe5c2ba2448fb200
Paul Richardson <p.g.richardson@redhat.com>
2014-01-09 14:15:44 +0000

    Implementation of the QueryService and QueryParser
    * QueryParser unites the differing versions of the TeiidParser by
      constructing a TeiidParserInstance of which the implementations will be
      either version 7 or 8.

---
+++ plugins/teiid/org.teiid.runtime.client/src/org/teiid/runtime/client/sql/QueryParser.java
@@ -0,0 +1,198 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
+
+package org.teiid.runtime.client.sql;
+
+import java.io.Reader;
+import java.io.StringReader;
+import org.teiid.designer.runtime.version.spi.ITeiidServerVersion;
+import org.teiid.runtime.client.lang.ParseInfo;
+import org.teiid.runtime.client.lang.ast.Command;
+import org.teiid.runtime.client.lang.ast.Criteria;
+import org.teiid.runtime.client.lang.ast.Expression;
+import org.teiid.runtime.client.lang.parser.TeiidParser;
+import org.teiid.runtime.client.lang.parser.v7.Teiid7Parser;
+import org.teiid.runtime.client.lang.parser.v8.Teiid8Parser;
+
+/**
+ * <p>Converts a SQL-string to an object version of a query.  This
+ * QueryParser can be reused but is NOT thread-safe as the parser uses an
+ * input stream.  Putting multiple queries into the same stream will result
+ * in unpredictable and most likely incorrect behavior.</p>
+ */
+public class QueryParser {
+
+	private TeiidParser teiidParser;
+
+    private ITeiidServerVersion teiidVersion;
+    
+	/**
+	 * Construct a QueryParser - this may be reused.
+	 *
+	 * @param teiidVersion
+	 */
+	public QueryParser(ITeiidServerVersion teiidVersion) {
+	    this.teiidVersion = teiidVersion;
+	    this.teiidParser = createTeiidParser(new StringReader("")); //$NON-NLS-1$
+	}
+
+	/**
+     * @return the teiidParser
+     */
+    public TeiidParser getTeiidParser() {
+        return this.teiidParser;
+    }
+
+	/**
+	 * Helper method to get a TeiidParser instance for given sql string.
+	 */
+	private TeiidParser getSqlParser(String sql) {
+		return getSqlParser(new StringReader(sql));
+	}
+
+	private TeiidParser createTeiidParser(Reader sql) {
+	    if (sql == null)
+	        throw new IllegalArgumentException();
+
+	    int major = Integer.parseInt(teiidVersion.getMajor());
+
+	    TeiidParser teiidParser = null;
+	    switch (major) {
+	        case 7:
+	            teiidParser = new Teiid7Parser(sql);
+	            break;
+	        case 8:
+	            teiidParser = new Teiid8Parser(sql);
+	            break;
+	        default:
+	            throw new UnsupportedOperationException();
+	    }
+
+	    return teiidParser;
+	}
+
+	private TeiidParser getSqlParser(Reader sql) {
+		if(teiidParser == null) {
+		    teiidParser = createTeiidParser(sql);
+		} else
+		    teiidParser.ReInit(sql);
+
+		return teiidParser;
+	}
+
+	/**
+	 * Takes a SQL string representing a Command and returns the object
+	 * representation.
+	 *
+	 * @param sql SQL string 
+	 * instead of string litral
+	 * @return SQL object representation
+	 * @throws Exception if parsing fails
+	 * @throws IllegalArgumentException if sql is null
+	 */	
+	public Command parseCommand(String sql) throws Exception {
+	    return parseCommand(sql, new ParseInfo(), false);
+	}
+
+    /**
+     * Takes a SQL string representing a Command and returns the object
+     * representation.
+     *
+     * @param sql
+     * @return SQL object representation
+     * @throws Exception if parsing fails
+     * @throws IllegalArgumentException if sql is null
+     */
+    public Command parseDesignerCommand(String sql) throws Exception {
+        return parseCommand(sql, new ParseInfo(), true);
+    }
+
+	private Command parseCommand(String sql, ParseInfo parseInfo, boolean designerCommands) throws Exception {
+        if(sql == null || sql.length() == 0) {
+             throw new IllegalArgumentException();
+        }
+        
+    	Command result = null;
+        try{
+            if (designerCommands) {
+                result = getSqlParser(sql).designerCommand(parseInfo);
+            } else {
+                result = getSqlParser(sql).command(parseInfo);
+            }
+        } catch(Exception e) {
+            throw e;
+        }
+
+		return result;
+	}
+
+    /**
+     * Takes a SQL string representing an SQL criteria (i.e. just the WHERE
+     * clause) and returns the object representation.
+     * @param sql SQL criteria (WHERE clause) string
+     * @return Criteria SQL object representation
+     * @throws Exception if parsing fails
+     * @throws IllegalArgumentException if sql is null
+     */
+    public Criteria parseCriteria(String sql) throws Exception {
+        if(sql == null) {
+            throw new IllegalArgumentException();
+        }
+
+        ParseInfo dummyInfo = new ParseInfo();
+
+        Criteria result = null;
+        try{
+            result = getSqlParser(sql).criteria(dummyInfo);
+
+        } catch(Exception e) {
+            throw e;
+        }
+
+        return result;
+    }
+        
+    /**
+     * Takes a SQL string representing an SQL expression
+     * and returns the object representation.
+     * @param sql SQL expression string
+     * @return SQL expression object
+     * @throws Exception if parsing fails
+     * @throws IllegalArgumentException if sql is null
+     */
+    public Expression parseExpression(String sql) throws Exception {
+        if(sql == null) {
+            throw new IllegalArgumentException();
+        }
+
+        ParseInfo dummyInfo = new ParseInfo();
+
+        Expression result = null;
+        try{
+            result = getSqlParser(sql).expression(dummyInfo);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        return result;
+    }
+}
