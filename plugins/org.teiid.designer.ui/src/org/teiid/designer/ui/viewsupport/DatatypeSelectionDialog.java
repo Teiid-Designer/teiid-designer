132e6e79df93599e498a0feb9e722971d8b5f36b
Barry LaFond <blafond@redhat.com>
2014-10-01 15:20:32 -0500

    TEIIDDES-2350 increased initial width of columns

--- plugins/org.teiid.designer.ui/src/org/teiid/designer/ui/viewsupport/DatatypeSelectionDialog.java
+++ plugins/org.teiid.designer.ui/src/org/teiid/designer/ui/viewsupport/DatatypeSelectionDialog.java
@@ -390,17 +390,17 @@ public class DatatypeSelectionDialog extends ListDialog implements UiConstants {
         TableViewerColumn column = new TableViewerColumn(tableViewer, SWT.LEFT);
         column.getColumn().setText("Design-time Type"); //$NON-NLS-1$
         column.setLabelProvider(new DatatypeLabelProvider(0));
-        column.getColumn().pack();
+        column.getColumn().setWidth(140);
 
         column = new TableViewerColumn(tableViewer, SWT.LEFT);
         column.getColumn().setText("Run-time Type" + "          "); //$NON-NLS-1$
         column.setLabelProvider(new DatatypeLabelProvider(1));
-        column.getColumn().pack();
+        column.getColumn().setWidth(140);
         
         column = new TableViewerColumn(tableViewer, SWT.LEFT);
         column.getColumn().setText("Base Type" + "                        "); //$NON-NLS-1$
         column.setLabelProvider(new DatatypeLabelProvider(2));
-        column.getColumn().pack();
+        column.getColumn().setWidth(140);
 
         // attach the filters:
         textFilter.attachToViewer(tableViewer, true);
