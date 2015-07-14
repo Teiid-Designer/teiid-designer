c8d699a46db7affbedb40d437d3492488244fb81
Ted Jones <tejones@redhat.com>
2015-07-14 16:01:41 -0500

    teiiddes-2561-9.0.x: Fix for running Swagger/generated REST war  on remote servers. Upgraded Swagger and added relative URL. Also upgraded Swagger UI.

--- plugins/org.teiid.designer.webservice.lib/rest_war_resources/webapps/WEB-INF/classes/org/teiid/rest/services/TeiidRestApplication.java
+++ plugins/org.teiid.designer.webservice.lib/rest_war_resources/webapps/WEB-INF/classes/org/teiid/rest/services/TeiidRestApplication.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
 package org.teiid.rest.services;
 
 import java.util.HashSet;
@@ -6,7 +27,7 @@ import javax.ws.rs.core.Application;
 
 public class TeiidRestApplication extends Application {
     private Set<Object> singletons = new HashSet<Object>();
-    private Set<Class<?>> empty = new HashSet<Class<?>>();
+    private Set<Class<?>> resources = new HashSet<Class<?>>();
 
     public TeiidRestApplication() {
         ${resources}
@@ -14,7 +35,11 @@ public class TeiidRestApplication extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        return empty;
+    	resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ResourceListingProvider.class);
+        return resources;
     }
     
     @Override
