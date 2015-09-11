776dc46f117e0cf0495aa20958159a2b04f5f522
blafond <blafond@redhat.com>
2015-09-11 09:39:23 -0500

    TEIIDDES-2627 fix to enable proper target project behavior when launched from Guides View or Cheat Sheets

--- plugins/org.teiid.designer.transformation.ui/src/org/teiid/designer/transformation/ui/wizards/file/TeiidMetadataImportWizard.java
+++ plugins/org.teiid.designer.transformation.ui/src/org/teiid/designer/transformation/ui/wizards/file/TeiidMetadataImportWizard.java
@@ -234,9 +234,12 @@ public class TeiidMetadataImportWizard extends AbstractWizard implements
 		
         if (this.folder == null) {
             // Check for Sources and View Folder from property definitions
-    		IContainer project = DesignerPropertiesUtil.getProject(designerProperties);
+    		IProject project = DesignerPropertiesUtil.getProject(designerProperties);
             IContainer srcFolderResrc = DesignerPropertiesUtil.getSourcesFolder(this.designerProperties);
             IContainer viewFolderResrc = DesignerPropertiesUtil.getViewsFolder(this.designerProperties);
+            if( project != null ) {
+            	getFileInfo().setTargetProject(project);
+            }
             if (srcFolderResrc != null) {
                 getFileInfo().setSourceModelLocation(srcFolderResrc.getFullPath());
             } else if( project != null ) {
