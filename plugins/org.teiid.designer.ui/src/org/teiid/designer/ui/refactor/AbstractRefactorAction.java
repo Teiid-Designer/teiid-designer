01c81c4ae81b70aa7587b83aa9a876147217ebf5
blafond <blafond@redhat.com>
2016-09-16 07:57:16 -0500

    TEIIDDES-2654 filtering IProjects from refactoring actions. Disabling Rename Model Project

--- plugins/org.teiid.designer.ui/src/org/teiid/designer/ui/refactor/AbstractRefactorAction.java
+++ plugins/org.teiid.designer.ui/src/org/teiid/designer/ui/refactor/AbstractRefactorAction.java
@@ -8,6 +8,8 @@
 package org.teiid.designer.ui.refactor;
 
 import java.util.List;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -77,6 +79,12 @@ public abstract class AbstractRefactorAction implements IWorkbenchWindowActionDe
         }
 
         List<IResource> resources = SelectionUtilities.getSelectedIResourceObjects(selection);
+        for( IResource resource : resources ) {
+        	if( resource instanceof IProject ) {
+        		action.setEnabled(false);
+        		return;
+        	}
+        }
 
         action.setEnabled(true);
         selectedResources = resources;
