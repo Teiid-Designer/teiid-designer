300de727b34ad6dce29dc8ee19f4f20c683ffe8f
blafond <blafond@redhat.com>
2016-10-03 11:19:40 -0500

    TEIIDDES-2921 fixed setting New INDEX NIS

--- plugins/org.teiid.designer.relational/src/org/teiid/designer/relational/model/RelationalModelFactory.java
+++ plugins/org.teiid.designer.relational/src/org/teiid/designer/relational/model/RelationalModelFactory.java
@@ -946,7 +946,7 @@ public class RelationalModelFactory implements RelationalConstants {
         
         Index index = FACTORY.createIndex();
         index.setName(indexRef.getName());
-        index.setNameInSource(index.getNameInSource());
+        index.setNameInSource(indexRef.getNameInSource());
         index.setFilterCondition(indexRef.getFilterCondition());
         index.setAutoUpdate(indexRef.isAutoUpdate());
         index.setNullable(indexRef.isNullable());
