803312c7cf3d991ed7012498e7d3dabaa0b1de40
Barry LaFond <blafond@redhat.com>
2017-04-04 15:20:25 -0500

    TEIIDDES-2795
     * added validation rule logic to check for connection:URL or connection:FlatFileHomeUrl property values for spaces in URL
     * add problem/warning marker so user is aware of potential problems on some operating systems

--- plugins/org.teiid.designer.core/src/org/teiid/designer/core/metamodel/aspect/core/aspects/validation/rules/ModelAnnotationUuidRule.java
+++ plugins/org.teiid.designer.core/src/org/teiid/designer/core/metamodel/aspect/core/aspects/validation/rules/ModelAnnotationUuidRule.java
@@ -7,9 +7,12 @@
  */
 package org.teiid.designer.core.metamodel.aspect.core.aspects.validation.rules;
 
+import java.util.Properties;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
 import org.teiid.core.designer.util.CoreArgCheck;
+import org.teiid.core.designer.util.StringConstants;
 import org.teiid.designer.core.ModelEditor;
 import org.teiid.designer.core.ModelerCore;
 import org.teiid.designer.core.validation.ObjectValidationRule;
@@ -18,6 +21,10 @@ import org.teiid.designer.core.validation.ValidationProblem;
 import org.teiid.designer.core.validation.ValidationProblemImpl;
 import org.teiid.designer.core.validation.ValidationResult;
 import org.teiid.designer.core.validation.ValidationResultImpl;
+import org.teiid.designer.core.workspace.ModelResource;
+import org.teiid.designer.core.workspace.ModelUtil;
+import org.teiid.designer.core.workspace.ModelWorkspaceException;
+import org.teiid.designer.core.workspace.ResourceAnnotationHelper;
 import org.teiid.designer.metamodels.core.ModelAnnotation;
 
 
@@ -60,6 +67,32 @@ public class ModelAnnotationUuidRule implements ObjectValidationRule {
 	            context.addUuidToContext(uuidString);
 	        }
         }
+		
+		// Add rule to check for "ENDPOINT" connection property?
+		
+		ResourceAnnotationHelper helper = new ResourceAnnotationHelper();
+		
+		try {
+			ModelResource mr = ModelUtil.getModel(eObject);
+			Properties modelProps = helper.getProperties(mr, "connection:");
+			for( Object key : modelProps.keySet() ) {
+				String keyStr = (String)key;
+				if( keyStr.equalsIgnoreCase("connection:URL") || keyStr.equalsIgnoreCase("connection:FlatFileHomeUrl") ) {
+					String value = (String)modelProps.get(key);
+					if( value.contains(StringConstants.SPACE) ) {
+						final ValidationResult result = new ValidationResultImpl(eObject);
+						String modelName = editor.getModelName(eObject);
+						// create validation problem and addit to the result
+						final ValidationProblem problem  = new ValidationProblemImpl(0, IStatus.WARNING, ModelerCore.Util.getString("ModelAnnotationUuidRule.2",  value, modelName)); //$NON-NLS-1$
+						result.addProblem(problem);
+						context.addResult(result);
+					}
+				}
+			}
+		} catch (ModelWorkspaceException e) {
+			e.printStackTrace();
+		}
+		
     }
 
 }
